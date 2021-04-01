074721da5c06f9c28fa203664623802e52ac5272
mattysmith22 <mattysmith22@googlemail.com>
2021-04-01 19:33:22 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@883f964821b301e5477ef04408bc1137ac0f5700 🚀

--- files/functions/event/fn_eventGetIn-sqf-SummaryToolTips.js
+++ files/functions/event/fn_eventGetIn-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:functions/event/fn_eventGetIn.sqf",{54:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Event handler that sets up apache when someone gets inside.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:functions/event/fn_eventGetIn.sqf",{52:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Event handler that sets up apache when someone gets inside.</div></div>"});
