2794220077493ed49508befe5ea88b55e6ceb094
mattysmith22 <mattysmith22@googlemail.com>
2021-04-05 23:44:41 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@ad58010a877ecfb592e43c805a4e36d7a084ad69 🚀

--- files/functions/targeting/fn_targetingGetAcquisitionSource-sqf-SummaryToolTips.js
+++ files/functions/targeting/fn_targetingGetAcquisitionSource-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:functions/targeting/fn_targetingGetAcquisitionSource.sqf",{140:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gets the player\'s seat\'s aquisition source. Can optionally be used to find the other crewstation\'s seat</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:functions/targeting/fn_targetingGetAcquisitionSource.sqf",{138:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gets the player\'s seat\'s aquisition source. Can optionally be used to find the other crewstation\'s seat</div></div>"});
