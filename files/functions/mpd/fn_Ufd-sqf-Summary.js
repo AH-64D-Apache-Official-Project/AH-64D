11f99981a264b5ee271ed8fc59b22173c2e98864
mattysmith22 <mattysmith22@googlemail.com>
2021-06-17 22:45:24 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@b0cfc204cfe3f2a7218dc27ab687bb690fdef694 🚀

--- files/functions/mpd/fn_Ufd-sqf-Summary.js
+++ files/functions/mpd/fn_Ufd-sqf-Summary.js
@@ -1,1 +1,1 @@
-﻿NDFramePage.OnPageTitleLoaded("File:functions/mpd/fn_Ufd.sqf","fn_Ufd.sqf");NDSummary.OnSummaryLoaded("File:functions/mpd/fn_Ufd.sqf",[["SQF","SQF"]],[["Functions","Function"],["Groups","Group"]],[[117,0,1,"Functions","Functions"],[118,0,0,"fza_fnc_Ufd","fza_fnc_Ufd"]]);
+﻿NDFramePage.OnPageTitleLoaded("File:functions/mpd/fn_Ufd.sqf","fn_Ufd.sqf");NDSummary.OnSummaryLoaded("File:functions/mpd/fn_Ufd.sqf",[["SQF","SQF"]],[["Functions","Function"],["Groups","Group"]],[[115,0,1,"Functions","Functions"],[116,0,0,"fza_fnc_Ufd","fza_fnc_Ufd"]]);
