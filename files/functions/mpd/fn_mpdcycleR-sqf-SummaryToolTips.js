22a4a5fd82a05d7f9da61ef639c3996bbb9e08e2
mattysmith22 <mattysmith22@googlemail.com>
2021-08-05 20:05:16 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@596eaeafc8e693245ecc7478e1f7a9b6206dd729 🚀

--- files/functions/mpd/fn_mpdcycleR-sqf-SummaryToolTips.js
+++ files/functions/mpd/fn_mpdcycleR-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:functions/mpd/fn_mpdcycleR.sqf",{20:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">cycles through the right mpds</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:functions/mpd/fn_mpdcycleR.sqf",{22:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">cycles through the right mpds</div></div>"});
