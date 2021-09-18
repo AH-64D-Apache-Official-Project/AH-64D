1a34bf5f4686e0c59bd1797039346be0fa96d205
Rosd6(Dryden) <Doolieollie@gmail.com>
2021-09-18 20:08:47 +0100

    Uniform change

--- @AH-64D Apache Longbow/Addons/fza_ah64_controls/config/cfgVehicles.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_controls/config/cfgVehicles.hpp
@@ -15,15 +15,15 @@ class CfgVehicles
 		scope                   = 2;
 		scopeCurator            = 2;
 		displayName             = "AH-64D Pilot";
-		uniformClass			= "U_B_HeliPilotCoveralls";
+		uniformClass			= "U_B_CombatUniform_mcam";
 		weapons[]               = {"arifle_SPAR_01_blk_MRCO_Flash_F", "hgun_P07_blk_F", "Throw", "Put", "Binocular"};
 		respawnWeapons[]        = {"arifle_SPAR_01_blk_MRCO_Flash_F", "hgun_P07_blk_F", "Throw", "Put", "Binocular"};
 		Items[]                 = {"FirstAidKit", "FirstAidKit", "ToolKit"};
 		RespawnItems[]          = {"FirstAidKit", "FirstAidKit", "ToolKit"};
 		magazines[]             = {MAG_6(30Rnd_556x45_Stanag), MAG_2(16Rnd_9x21_Mag), MAG_2(B_IR_Grenade), MAG_2(SmokeShell), MAG_2(SmokeShellBlue), MAG_2(SmokeShellPurple), MAG_2(Chemlight_green)};
 		respawnMagazines[]      = {MAG_6(30Rnd_556x45_Stanag), MAG_2(16Rnd_9x21_Mag), MAG_2(B_IR_Grenade), MAG_2(SmokeShell), MAG_2(SmokeShellBlue), MAG_2(SmokeShellPurple), MAG_2(Chemlight_green)};
-		linkedItems[]           = {"V_PlateCarrier1_rgr", "H_PilotHelmetHeli_O", "G_Bandanna_tan", "ItemMap", "ItemGPS", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles_OPFOR"};
-		respawnLinkedItems[]    = {"V_PlateCarrier1_rgr", "H_PilotHelmetHeli_O", "G_Bandanna_tan", "ItemMap", "ItemGPS", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles_OPFOR"};
+		linkedItems[]           = {"V_PlateCarrier1_rgr", "H_PilotHelmetHeli_O", "ItemMap", "ItemGPS", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles_OPFOR"};
+		respawnLinkedItems[]    = {"V_PlateCarrier1_rgr", "H_PilotHelmetHeli_O", "ItemMap", "ItemGPS", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles_OPFOR"};
 		faction					= "fza_usaav";
 		vehicleclass			= "fza_Men";
 	};
