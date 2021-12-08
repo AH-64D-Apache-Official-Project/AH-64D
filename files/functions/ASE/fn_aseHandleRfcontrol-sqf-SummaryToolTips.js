cca494e7ebe56e1e03a938ee6b08d9bf249f3ed3
mattysmith22 <mattysmith22@googlemail.com>
2021-12-08 14:20:28 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@de01097d9f838bf8dbd18e51c9344de124df7a7c 🚀

--- files/functions/ASE/fn_aseHandleRfcontrol-sqf-SummaryToolTips.js
+++ files/functions/ASE/fn_aseHandleRfcontrol-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:functions/ASE/fn_aseHandleRfcontrol.sqf",{86:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Enables RF jammer, can be disabled by setting fza_ah64_rfJamOn to false.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:functions/ASE/fn_aseHandleRfcontrol.sqf",{84:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Enables RF jammer, can be disabled by setting fza_ah64_rfJamOn to false.</div></div>"});
