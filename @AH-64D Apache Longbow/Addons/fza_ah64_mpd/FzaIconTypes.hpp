c37c660a98b2bd8a3487121207f9a2bc341d5ae5
BradMick <bradMick@example.com>
2021-12-22 07:28:47 -0600

    Update
    Added FCR Icons and Textures

--- @AH-64D Apache Longbow/Addons/fza_ah64_mpd/FzaIconTypes.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_mpd/FzaIconTypes.hpp
@@ -11,6 +11,12 @@
 
 class FzaIconTypes {
 //--------------------Icon Type A--------------------
+//                          
+//                      +---------+
+//                      |         |
+//              000/AAA |   TEX   |  
+//                      |         |
+//                      +---------+  
     //Waypoints
     class CC {                                                  //Communication check point
         tex= "\fza_ah64_mpd\tex\tsdIcons\cc.paa";
@@ -24,7 +30,6 @@ class FzaIconTypes {
     class RP:CC  { tex= "\fza_ah64_mpd\tex\tsdIcons\rp.paa";};  //Release Point
     class SP:CC  { tex= "\fza_ah64_mpd\tex\tsdIcons\sp.paa";};  //Start Point
     class WP:CC  { tex= "\fza_ah64_mpd\tex\tsdIcons\wp.paa";};  //Waypoint
-
     //Hazards
     class TO {                                                  //Tower - Over 1000ft agl
         tex= "\fza_ah64_mpd\tex\tsdIcons\to.paa";
@@ -36,7 +41,6 @@ class FzaIconTypes {
     class TU:TO  { tex= "\fza_ah64_mpd\tex\tsdIcons\tu.paa";};  //Tower - Under 1000ft agl
     class WL:TO  { tex= "\fza_ah64_mpd\tex\tsdIcons\wl.paa";};  //Wires - Power transmission lines
     class WS:TO  { tex= "\fza_ah64_mpd\tex\tsdIcons\ws.paa";};  //Wires - Telephon & electric
-
     //Control Measures
     class AA {                                                  //Assembly area
         tex= "\fza_ah64_mpd\tex\tsdIcons\aa.paa";
@@ -77,14 +81,17 @@ class FzaIconTypes {
         tex= "\fza_ah64_mpd\tex\tsdIcons\mi.paa";
         color    = MPD_ICON_COLOR_CYAN;
     };
-
     //Target/Threats
 
 //--------------------Icon Type B--------------------
+//                          
+//                      +---------+
+//                      |         |
+//                      |   AAA   |  
+//                      |   TEX   |
+//                      +---------+                       
     //Waypoints
-
     //Hazards
-
     //Control Measures
     class FU {                                                  //Friendly Unit
         tex= "\fza_ah64_mpd\tex\tsdIcons\fu.paa";
@@ -97,14 +104,17 @@ class FzaIconTypes {
         tex= "\fza_ah64_mpd\tex\tsdIcons\eu.paa";
         color    = MPD_ICON_COLOR_RED;
     };
-
     //Target/Threats
 
 //--------------------Icon Type C--------------------
+//                          
+//                      +---------+
+//                      |         |
+//                      |   TEX   |
+//                      |   0 0   |
+//                      +---------+
     //Waypoints
-
     //Hazards
-
     //Control Measures
     class AP  {                                                 //Air Control Point
         tex= "\fza_ah64_mpd\tex\tsdIcons\ap.paa";
@@ -116,14 +126,17 @@ class FzaIconTypes {
     class BP:AP  { tex= "\fza_ah64_mpd\tex\tsdIcons\bp.paa";};  //Battle Position
     class FA:AP  { tex= "\fza_ah64_mpd\tex\tsdIcons\fa.paa";};  //Forward Assembly Area
     class HA:AP  { tex= "\fza_ah64_mpd\tex\tsdIcons\ha.paa";};  //Holding Area
-
     //Target/Threats
 
 //--------------------Icon Type D--------------------
+//                          AAA
+//                      +---------+
+//                      |         |
+//                   00 |   TEX   |
+//                      |         |
+//                      +---------+
     //Waypoints
-
     //Hazards
-
     //Control Measures
     class FC {                                                  //FARP - Fuel and Ammo
         tex= "\fza_ah64_mpd\tex\tsdIcons\fc.paa";
@@ -135,14 +148,17 @@ class FzaIconTypes {
     };
     class FF:FC  { tex= "\fza_ah64_mpd\tex\tsdIcons\ff.paa";};  //FARP - Fuel only
     class FM:FC  { tex= "\fza_ah64_mpd\tex\tsdIcons\fm.paa";};  //Farm - Ammo only
-
     //Target/Threats
 
 //--------------------Icon Type E--------------------
+//                          AAA
+//                      +---------+
+//                      |         |
+//                      |   TEX   |
+//                      |         |
+//                      +---------+
     //Waypoints
-
     //Hazards
-
     //Control Measures
     class AG  {                                                     //Airfield - General
         tex= "\fza_ah64_mpd\tex\tsdIcons\ag.paa";
@@ -158,21 +174,23 @@ class FzaIconTypes {
         tex= "\fza_ah64_mpd\tex\tsdIcons\idmsub.paa";
         color    = MPD_ICON_COLOR_CYAN;
     };
-
     //Target/Threats
  
 //--------------------Icon Type F--------------------
+//
+//                      +---------+
+//                      |         |
+//                      |   TEX   | 00
+//                      |         |
+//                      +---------+
     //Waypoints
-
     //Hazards
-
     //Control Measures
-
     //Target/Threats
     class TG  {
         tex= "\fza_ah64_mpd\tex\tsdIcons\tg.paa";
         color    = MPD_ICON_COLOR_RED;
-        type = POINT_TYPE_TG;
+        type     = POINT_TYPE_TG;
         iconType = MPD_ICON_TYPE_F;
         textA    = MPD_ICON_TEXT_ID;
     };
@@ -180,6 +198,12 @@ class FzaIconTypes {
     class TR:TG    { tex= "\fza_ah64_mpd\tex\tsdIcons\r.paa";};  
 
 //--------------------Icon Type G--------------------
+//
+//                      +---------+
+//                      | 0     0 |
+//                      |   TEX   |
+//                      |         |
+//                      +---------+
     //Waypoints
 
     //Hazards
@@ -214,3 +238,99 @@ class FzaIconTypes {
     class S6:GU { tex = "\fza_ah64_mpd\tex\tsdIcons\gu.paa";};
     class ZU:GU { tex = "\fza_ah64_mpd\tex\tsdIcons\gu.paa";};
 };
+
+//--------------------Icon Type H--------------------
+//
+//                      +---------+
+//                      |         |
+//                      |   TEX   |
+//                      |         |
+//                      +---------+
+/*
+    //--------------------FCR LOAL--------------------
+    //ADU
+    class FCR_ADU_LOAL { 
+        tex = "\fza_ah64_mpd\tex\fcrIcons\aduLOAL.paa";
+        type     = POINT_TYPE_FCR;
+        iconType = MPD_ICON_TYPE_F;
+    };
+    class FCR_ADU_LOAL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\aduLOAL_ANTS.paa";};
+    class FCR_ADU_LOAL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\aduLOAL_NTS.paa";};
+    class FCR_ADU_LOAL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\aduLOAL_NTS_NoMSL.paa";};
+    //Heli
+    class FCR_HELI_LOAL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\heliLOAL.paa";};
+    class FCR_HELI_LOAL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\heliLOAL_ANTS.paa";};
+    class FCR_HELI_LOAL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\heliLOAL_NTS.paa";};
+    class FCR_HELI_LOAL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\heliLOAL_NTS_NoMSL.paa";};
+    //Tracked
+    class FCR_TRACK_LOAL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\trackLOAL.paa";};
+    class FCR_TRACK_LOAL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\trackLOAL_ANTS.paa";};
+    class FCR_TRACK_LOAL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\trackLOAL_NTS.paa";};
+    class FCR_TRACK_LOAL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\trackLOAL_NTS_NoMSL.paa";};    
+    //Unknown
+    class FCR_UNK_LOAL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\unkLOAL.paa";};
+    class FCR_UNK_LOAL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\unkLOAL_ANTS.paa";};
+    class FCR_UNK_LOAL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\unkLOAL_NTS.paa";};
+    class FCR_UNK_LOAL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\unkLOAL_NTS_NoMSL.paa";}; 
+    //Wheeled
+    class FCR_WHEEL_LOAL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelLOAL.paa";};
+    class FCR_WHEEL_LOAL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelLOAL_ANTS.paa";};
+    class FCR_WHEEL_LOAL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelLOAL_NTS.paa";};
+    class FCR_WHEEL_LOAL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelLOAL_NTS_NoMSL.paa";}; 
+
+    //--------------------FCR LOBL--------------------
+    //ADU
+    class FCR_ADU_LOBL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\aduLOBL.paa";};
+    class FCR_ADU_LOBL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\aduLOBL_ANTS.paa";};
+    class FCR_ADU_LOBL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\aduLOBL_NTS.paa";};
+    class FCR_ADU_LOBL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\aduLOBL_NTS_NoMSL.paa";};
+    //Heli
+    class FCR_HELI_LOBL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\heliLOBL.paa";};
+    class FCR_HELI_LOBL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\heliLOBL_ANTS.paa";};
+    class FCR_HELI_LOBL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\heliLOBL_NTS.paa";};
+    class FCR_HELI_LOBL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\heliLOBL_NTS_NoMSL.paa";};
+    //Tracked
+    class FCR_TRACK_LOBL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\trackLOBL.paa";};
+    class FCR_TRACK_LOBL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\trackLOBL_ANTS.paa";};
+    class FCR_TRACK_LOBL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\trackLOBL_NTS.paa";};
+    class FCR_TRACK_LOBL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\trackLOBL_NTS_NoMSL.paa";};  
+    //Unknown
+    class FCR_UNK_LOBL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\unkLOBL.paa";};
+    class FCR_UNK_LOBL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\unkLOBL_ANTS.paa";};
+    class FCR_UNK_LOBL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\unkLOBL_NTS.paa";};
+    class FCR_UNK_LOBL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\unkLOBL_NTS_NoMSL.paa";}; 
+    //Wheeled
+    class FCR_WHEEL_LOBL:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelLOBL.paa";};
+    class FCR_WHEEL_LOBL_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelLOBL_ANTS.paa";};
+    class FCR_WHEEL_LOBL_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelLOBL_NTS.paa";};
+    class FCR_WHEEL_LOBL_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelLOBL_NTS_NoMSL.paa";}; 
+
+    //--------------------FCR MOVE--------------------
+    //ADU
+    class FCR_ADU_MOVE:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\aduMOVE.paa";};
+    class FCR_ADU_MOVE_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\aduMOVE_ANTS.paa";};
+    class FCR_ADU_MOVE_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\aduMOVE_NTS.paa";};
+    class FCR_ADU_MOVE_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\aduMOVE_NTS_NoMSL.paa";};
+    //Heli
+    class FCR_HELI_MOVE:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\heliMOVE.paa";};
+    class FCR_HELI_MOVE_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\heliMOVE_ANTS.paa";};
+    class FCR_HELI_MOVE_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\heliMOVE_NTS.paa";};
+    class FCR_HELI_MOVE_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\heliMOVE_NTS_NoMSL.paa";};
+    //Tracked
+    class FCR_TRACK_MOVE:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\trackMOVE.paa";};
+    class FCR_TRACK_MOVE_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\trackMOVE_ANTS.paa";};
+    class FCR_TRACK_MOVE_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\trackMOVE_NTS.paa";};
+    class FCR_TRACK_MOVE_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\trackMOVE_NTS_NoMSL.paa";};      
+    //Unknown
+    class FCR_UNK_MOVE:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\unkMOVE.paa";};
+    class FCR_UNK_MOVE_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\unkMOVE_ANTS.paa";};
+    class FCR_UNK_MOVE_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\unkMOVE_NTS.paa";};
+    class FCR_UNK_MOVE_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\unkMOVE_NTS_NoMSL.paa";}; 
+    //Wheeled
+    class FCR_WHEEL_MOVE:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelMOVE.paa";};
+    class FCR_WHEEL_MOVE_ANTS:FCR_ADU_LOAL      { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelMOVE_ANTS.paa";};
+    class FCR_WHEEL_MOVE_NTS:FCR_ADU_LOAL       { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelMOVE_NTS.paa";};
+    class FCR_WHEEL_MOVE_NTS_NOMSL:FCR_ADU_LOAL { tex = "\fza_ah64_mpd\tex\fcrIcons\wheelMOVE_NTS_NoMSL.paa";}; 
+    //Flyer
+    class FCR_FLYER_MOVE:FCR_ADU_LOAL           { tex = "\fza_ah64_mpd\tex\fcrIcons\flyer.paa";};
+    */
