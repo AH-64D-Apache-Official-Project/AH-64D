33bb0d6dce01762e44af4be70e3b43ee5bec7b18
Rosd6(Dryden) <Doolieollie@gmail.com>
2021-12-29 21:44:46 +0000

    Base

---
+++ @AH-64D Apache Longbow/Addons/fza_ah64_controls/config/CfgUi.hpp
@@ -0,0 +1,108 @@
+class RscInGameUI
+{
+    class RscUnitInfoAir;
+    class fza_ah64_rscInfoMinimal: RscUnitInfoAir
+    {
+        onLoad="";
+		controls[] = {
+            "CA_BackgroundVehicle",
+            "CA_BackgroundVehicleTitle",
+            "CA_BackgroundVehicleTitleDark",
+            "CA_BackgroundFuel",
+            "CA_Vehicle",
+            "CA_VehicleRole",
+            "CA_HitZones",
+            "CA_VehicleTogglesBackground",
+            "CA_VehicleToggles",
+            "CA_SpeedBackground",
+            "CA_SpeedUnits",
+            "CA_Speed",
+            "CA_ValueFuel",
+            "CA_AltBackground",
+            "CA_AltUnits",
+            "CA_Alt",
+            "WeaponInfoControlsGroupRight"
+        };
+    };
+    class RscUnitInfoAirRTDBasic;
+    class fza_ah64_rscInfoBasic: RscUnitInfoAirRTDBasic
+    {
+        onLoad="";
+        controls[] = {
+        };
+		controlsx[] = {
+            "WeaponInfoControlsGroupRight",
+            "CA_SpeedBackground",
+            "CA_TextSpeed",
+            "CA_Speed",
+            "CA_AltBackground",
+            "CA_TextAlt",
+            "CA_Alt",
+            "CA_HeadingBackground",
+            "CA_Heading",
+            "CA_ValueColective"
+        };
+    };
+    class RscUnitInfoAirRTDFull;
+    class fza_ah64_rscInfoRTD: RscUnitInfoAirRTDFull
+    {
+        onLoad="";
+        controls[] = {
+            "CA_BackgroundVehicle",
+            "CA_BackgroundVehicleTitle",
+            "CA_BackgroundVehicleTitleDark",
+            "CA_BackgroundFuel",
+            "CA_Vehicle",
+            "CA_VehicleRole",
+            "CA_HitZones",
+            "CA_VehicleTogglesBackground",
+            "CA_VehicleToggles",
+            "CA_SpeedBackground",
+            "CA_SpeedUnits",
+            "CA_Speed",
+            "CA_ValueFuel",
+            "CA_AltBackground",
+            "CA_AltUnits",
+            "CA_Alt",
+            "CA_Horizon_Lite",
+            "CA_Speed_Analogue",
+            "CA_Altitude_Analogue",
+            "CA_Horizon_Analogue",
+            "CA_Stability_Analogue",
+            "CA_Compass_Analogue",
+            "WeaponInfoControlsGroupRight"
+        };
+    };
+};
+
+
+
+
+
+
+    
+/* Original
+    "CA_BackgroundVehicle",
+    "CA_BackgroundVehicleTitle",
+    "CA_BackgroundVehicleTitleDark",
+    "CA_BackgroundFuel",
+    "CA_Vehicle",
+    "CA_VehicleRole",
+    "CA_HitZones",
+    "CA_VehicleTogglesBackground",
+    "CA_VehicleToggles",
+    "CA_SpeedBackground",
+    "CA_SpeedUnits",
+    "CA_Speed",
+    "CA_ValueFuel",
+    "CA_AltBackground",
+    "CA_AltUnits",
+    "CA_Alt",
+    "CA_Horizon_Lite",
+    "CA_Speed_Analogue",
+    "CA_Altitude_Analogue",
+    "CA_Horizon_Analogue",
+    "CA_Stability_Analogue",
+    "CA_Compass_Analogue",
+    "WeaponInfoControlsGroupRight"
+*/
