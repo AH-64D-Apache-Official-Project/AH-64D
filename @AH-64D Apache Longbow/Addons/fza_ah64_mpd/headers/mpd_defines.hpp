9f2346c64f3ba6a47aeda4ea49af02a9db81ced2
Matthew Smith <mattysmith22@googlemail.com>
2022-01-03 17:48:19 +0000

    Added additional documentation mpd_defines

--- @AH-64D Apache Longbow/Addons/fza_ah64_mpd/headers/mpd_defines.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_mpd/headers/mpd_defines.hpp
@@ -1,4 +1,12 @@
-// Text constants
+/* Constants: Text size constants
+  MPD_TEXT_HEIGHT - Height of a single normal character
+  MPD_TEXT_WIDTH - Width of a single normal character
+  MPD_TEXT_WIDTH_VEC - Width to put into type="text" for a character with width MPD_TEXT_WIDTH
+  MPD_TEXT_SMALL_PAD - Padding offset when using small text
+  MPD_TEXT_SMALL_HEIGHT - Height of a single small character
+  MPD_TEXT_SMALL_WIDTH - Width of a single small character
+  MPD_TEXT_SMALL_WIDTH_VEC - Width to put into type="text" for a character with width MPD_TEXT_SMALL_WIDTH
+*/
 #define MPD_TEXT_HEIGHT 0.045
 #define MPD_TEXT_WIDTH 0.0205
 #define MPD_TEXT_WIDTH_VEC 0.045
@@ -8,14 +16,38 @@
 #define MPD_TEXT_SMALL_WIDTH 0.018
 #define MPD_TEXT_SMALL_WIDTH_VEC 0.03
 
-// Drawing constants - used by macros below
+/* Constants: Misc size constants
+  MPD_ARROW_PAD - Offset of an arrow above an MPD text character
+  MPD_ARROW_LENGTH - The length of the solid triangle in an arrow
+  MPD_ARROW_HEIGHT - The height of an MPD arrow
+  MPD_BOX_PAD_X - How much a box needs to be padded in the x-direction on each side
+  MPD_BOX_PAD_Y - How much a box needs to be padded in the y-direction on each side
+*/
 #define MPD_ARROW_PAD 0.007
 #define MPD_ARROW_LENGTH 0.01
 #define MPD_ARROW_HEIGHT 0.006
 #define MPD_BOX_PAD_X 0.006
 #define MPD_BOX_PAD_Y 0.000
 
-// Common MPD position coordinates - edges of MPDs next to buttons
+/* Constants: MPD VAB positions
+  MPD_POS_BUTTON_L_X - X coordinate of VABS L[1-6]
+  MPD_POS_BUTTON_R_X - X coordinate of VABS R[1-6]
+  MPD_POS_BUTTON_LR_1_Y - Y coordinate of VABS [LR]1
+  MPD_POS_BUTTON_LR_2_Y - Y coordinate of VABS [LR]2
+  MPD_POS_BUTTON_LR_3_Y - Y coordinate of VABS [LR]3
+  MPD_POS_BUTTON_LR_4_Y - Y coordinate of VABS [LR]4
+  MPD_POS_BUTTON_LR_5_Y - Y coordinate of VABS [LR]5
+  MPD_POS_BUTTON_LR_6_Y - Y coordinate of VABS [LR]6
+
+  MPD_POS_BUTTON_T_Y - Y coordinate of VABS T[1-6]
+  MPD_POS_BUTTON_B_Y - Y coordinate of VABS B[1-6]
+  MPD_POS_BUTTON_TB_1_X - Y coordinate of VABS [TB]1
+  MPD_POS_BUTTON_TB_2_X - Y coordinate of VABS [TB]2
+  MPD_POS_BUTTON_TB_3_X - Y coordinate of VABS [TB]3
+  MPD_POS_BUTTON_TB_4_X - Y coordinate of VABS [TB]4
+  MPD_POS_BUTTON_TB_5_X - Y coordinate of VABS [TB]5
+  MPD_POS_BUTTON_TB_6_X - Y coordinate of VABS [TB]6
+*/
 #define MPD_POS_BUTTON_L_X 0.03
 #define MPD_POS_BUTTON_R_X 0.97
 #define MPD_POS_BUTTON_LR_1_Y 0.2
@@ -34,28 +66,118 @@
 #define MPD_POS_BUTTON_TB_5_X 0.69
 #define MPD_POS_BUTTON_TB_6_X 0.81
 
-#define COMMA ,
-#define CONCAT_2(x,y) x##y
-#define CONCAT_3(x,y,z) x##y##z
+// Group: Misc Macros
 
-// Source scales
+/* Macro: COMMA
+    Macro that evaluates to a comma, to be used when using a comma directly breaks another macro
+*/
+#define COMMA ,
+/* Macro: CONCAT_2
+    Directly concatenate two values
+
+Parameters:
+    x,y - arguments to concatenate
+*/
+#define CONCAT_2(x,y) x##y
+/* Macro: CONCAT_3
+    Directly concatenate three values
+
+Parameters:
+    x,y,z - arguments to concatenate
+*/
+#define CONCAT_3(x,y,z) x##y##z
+/* Macro: STRINGIFY
+    Takes input and stringifies it:
+
+    STRINGIFY(this is a test) => "this is a test"
+*/
+#define STRINGIFY(str) #str
+
+/* Constants: Source scales
+  MPD_SCALE_METERS_FEET - Conversion from meters to feet
+  MPD_SCALE_MPS_KNOTS - Conversion from meters per second to knots
+  MPD_SCALE_METERS_PER_SECOND_FEET_PER_MINUTE - Conversions from meters per second to feet per minute
+  MPD_SCALE_RADIANS_DEGREES - Conversion fom radians to degrees
+  MPD_SCALE_METERS_KM - Conversion from meters to KM
+*/
 #define MPD_SCALE_METERS_FEET 3.28084
 #define MPD_SCALE_MPS_KNOTS 1.94
 #define MPD_SCALE_METERS_PER_SECOND_FEET_PER_MINUTE 196.85
 #define MPD_SCALE_RADIANS_DEGREES 57.2958
 #define MPD_SCALE_METERS_KM 0.001
 
+// Group: Conditions
+
+/* Macro: EQ
+    Ensures two values are equal
+
+Parameters:
+    a,b - values to check are equal
+*/
 #define EQ(A,B) (((A)>((B) - 1))*((A)<((B) + 1)))
+/* Macro: USER_NUM
+    MFD user number to be used in conditions. Does not add MPD_OFFSET, and therefore cannot be used for MPD actions
+
+Parameters:
+    num - User number
+*/
 #define USER_NUM(num) user##num
+/* Macro: USER_NUM
+    MPD user number to be used in conditions. Adds MPD_OFFSET, and therefore should be used for MPD actions
+
+Parameters:
+    num - User number
+*/
+#define MPD_COND_USER(num) __EVAL("user" + str (MFD_OFFSET + num))
 
 #define MFD_USER_NUM(num) __EVAL(MFD_OFFSET + num)
-#define MPD_TEXT_STATIC(str)  source = static; text = str; sourceScale = 1;
-#define MPD_TEXT_USER_SCALE(num, scale) source = userText; sourceIndex = MFD_USER_NUM(num); sourceScale = 1;
-#define MPD_TEXT_USER(num) MPD_TEXT_USER_SCALE(num,1)
-#define MPD_NUM_USER(num) source = "user"; sourceIndex = MFD_USER_NUM(num);
-#define MPD_COND_USER(num) __EVAL("user" + str (MFD_OFFSET + num))
-#define STRINGIFY(str) #str
+// Group: Text Source Macros
 
+/* Macro: MPD_TEXT_STATIC
+    Sets static source in type="text"
+
+Parameters:
+    str - Static text to display. e.g: "test"
+*/
+#define MPD_TEXT_STATIC(str)  source = static; text = str; sourceScale = 1;
+
+/* Macro: MPD_TEXT_USER_SCALE
+    Sets user source in type="text", with a scaling factor
+
+Parameters:
+    num - MPD user string source
+    scale - Scaling to apply to input.
+*/
+#define MPD_TEXT_USER_SCALE(num, scale) source = userText; sourceIndex = MFD_USER_NUM(num); sourceScale = 1;
+
+/* Macro: MPD_TEXT_USER
+    Version of <MPD_TEXT_USER_SCALE> with no scaling factor
+
+Parameters:
+    num: MPD user string source
+*/
+#define MPD_TEXT_USER(num) MPD_TEXT_USER_SCALE(num,1)
+
+// Group: Float Source Macros
+
+/* Macro: MPD_NUM_USER
+    Sets float source to a numeric value
+
+Parameters:
+    num - MPD user float source
+*/
+#define MPD_NUM_USER(num) source = "user"; sourceIndex = MFD_USER_NUM(num);
+
+// Group: Standard draws
+
+/* Macro: MPD_POINTS_CURVED_CORNER
+    Draws a 90 degree curved corner about a point
+    
+Parameters:
+    bone - A bone to transform the corner by. "Null" if none.
+    posX, posY - point to draw the corner about
+    vecX, vecY - vector signifying the direction and size of the corner
+*/
 #define MPD_POINTS_CURVED_CORNER(bone, posX, posY, vecX, vecY) \
     {bone, {__EVAL((posX) + (vecX) * cos 0),  __EVAL((posY) + (vecY) * sin 0)}, 1}, \
     {bone, {__EVAL((posX) + (vecX) * cos 10), __EVAL((posY) + (vecY) * sin 10)}, 1}, \
@@ -69,6 +191,18 @@
     {bone, {__EVAL((posX) + (vecX) * cos 90), __EVAL((posY) + (vecY) * sin 90)}, 1}
 
 #define CURVED_BOX_PAD 0.0075
+/* Macro: MPD_POINTS_BOX_PAD
+    Draws a box, with curved corners, with a given dimensions and padding (curved corner side).
+    
+    Please note the box dimensions described are internal from the corners, so the real size will have 2*pad added in
+    each dimension. For a macro that counts the corners in the dimensions, use <MPD_POINTS_BOX_PAD_EXTERNAL>.
+    
+Parameters:
+    bone - A bone to transform the box  by. "Null" if none.
+    posX,posY - Top left corner of the box
+    width,height - Box dimensions (internal)
+    pad - size of the corners
+*/
 #define MPD_POINTS_BOX_PAD(bone, posX, posY, width, height, pad) \
     MPD_POINTS_CURVED_CORNER(bone, posX, posY, -pad, -pad), {}, \
     MPD_POINTS_CURVED_CORNER(bone, posX + width, posY, pad, -pad), {}, \
@@ -79,9 +213,35 @@
     {bone, {posX, posY - pad}, 1}, {bone, {posX + width, posY - pad}, 1}, {}, \
     {bone, {posX, posY + height + pad}, 1}, {bone, {posX + width, posY + height + pad}, 1}
 
+/* Macro: MPD_POINTS_BOX_PAD_EXTERNAL
+    Same as <MPD_POINTS_BOX_PAD>, but with the dimensions being measured including the corners.
+
+Parameters:
+    bone - A bone to transform the box  by. "Null" if none.
+    posX,posY - Top left corner of the box
+    width,height - Box dimensions (internal)
+    pad - size of the corners
+*/
 #define MPD_POINTS_BOX_PAD_EXTERNAL(bone, posX, posY, width, height, radius) MPD_POINTS_BOX_PAD(bone, (posX+radius), (posY+radius), (width-2*radius), (height-2*radius), radius)
 
+/* Macro: MPD_POINTS_BOX
+    Variant of <MPD_POINTS_BOX_PAD> that uses a standard padding amount. Dimensions specified are internal
+
+Parameters:
+    bone - A bone to transform the box  by. "Null" if none.
+    posX,posY - Top left corner of the box
+    width,height - Box dimensions (internal)
+*/
 #define MPD_POINTS_BOX(bone, posX, posY, width, height) MPD_POINTS_BOX_PAD(bone, posX, posY, width, height, CURVED_BOX_PAD)
+
+/* Macro: MPD_POINTS_BOX_POLY
+    Defines a polygon to be used in type="polygon" to occlude for a <MPD_POINTS_BOX>.
+
+Parameters:
+    bone - A bone to transform the box  by. "Null" if none.
+    posX,posY - Top left corner of the box
+    width,height - Box dimensions (internal)
+*/
 #define MPD_POINTS_BOX_POLY(bone, posX, posY, width, height) {\
     {Null, {posX-CURVED_BOX_PAD,posY-CURVED_BOX_PAD}, 1},\
     {Null, {posX+width+CURVED_BOX_PAD, posY-CURVED_BOX_PAD}, 1},\
@@ -89,6 +249,14 @@
     {Null, {posX-CURVED_BOX_PAD, posY+height+CURVED_BOX_PAD}, 1},\
 }
 
+/* Macro: MPD_CIRCLE
+    Defines a polygon to be used in type="polygon" to occlude for a <MPD_POINTS_BOX>.
+
+Parameters:
+    bone - A bone to transform the box  by. "Null" if none.
+    posX,posY - Top left corner of the box
+    width,height - Box dimensions (internal)
+*/
 #define MPD_CIRCLE(name, bone, radius) class name##Circle { \
     type = polygon; \
     points[] = { \
@@ -125,6 +293,33 @@
     }; \
 };
 
+// Group: Text & Text-related drawing
+
+/* About: Coordinate Positioning
+
+    Each of these macros has 3 variants, _L, _C and _R. This sets the coordinate used to draw from.
+
+    - _L spefifies that the point is in the top right corner, and the macro should be drawn to the left
+    - _C specifies that the point is in the top middle corner, and the macro should be drawn outwards
+    - _R specifies that the point is in the top left corner, and the macro should be drawn to the right
+
+    In situations where all 3 are a possibility, only one will be documented - MACRO_x
+
+    --- Code
+           \/     |     \/
+     MACRO_L   MACRO_C   MACRO_R
+    --- Code
+*/
+
+/* Macro: MPD_ARROW_x
+    Draws an arrow over text with the coordinate specified.
+
+Parameters:
+    name - Unique name to use in the class definition. Can be the same as that in <MPD_TEXT_x> as they are
+        deconflicted within the macros
+    startX, startY - Position of the text the arrow should be over
+    numChars - Number of characters in the text to draw an arrow over, defines the length of the arrow.
+*/
 #define MPD_ARROW_R(name, startX, startY, numChars) class Mpd_Arrow_##name##_Line { \
     type = line; \
     points[] = { \
@@ -142,6 +337,15 @@ class Mpd_Arrow_##name##_Triangle { \
         };\
 };
 
+/* Macro: MPD_BOX_x
+    Draws an box around text with the coordinate specified.
+
+Parameters:
+    name - Unique name to use in the class definition. Can be the same as that in <MPD_TEXT_x> as they are
+        deconflicted within the macros
+    startX, startY - Position of the text the box should be drawn around
+    numChars - Number of characters in the text to draw a box around, defines the size of the box
+*/
 #define MPD_BOX_R(name, startX, startY, numChars) class Mpd_Box_##name##_Line { \
     type = line; \
     width = 2; \
@@ -153,6 +357,16 @@ class Mpd_Arrow_##name##_Triangle { \
         {{startX - MPD_BOX_PAD_X, startY - MPD_BOX_PAD_Y}, 1}}; \
 };
 
+/* Macro: MPD_BOX_TALL_x
+    Draws an box around text with the coordinate specified. The box is taller than that drawn with <MPD_BOX_x>
+    and can fit an arrow within.
+
+Parameters:
+    name - Unique name to use in the class definition. Can be the same as that in <MPD_TEXT_x> as they are
+        deconflicted within the macros
+    startX, startY - Position of the text the box should be drawn around
+    numChars - Number of characters in the text to draw a box around, defines the size of the box
+*/
 #define MPD_BOX_TALL_R(name, startX, startY, numChars) class Mpd_Box_##name##_Line { \
     type = line; \
     width = 2; \
@@ -182,14 +396,42 @@ class Mpd_Arrow_##name##_Triangle { \
     config \
 };
 
+/* Macro: MPD_BOX_BONE_x
+    Draws text in the given direction, with the text content defined by config. Text is transformed by the given bone.
+
+Parameters:
+    name - Unique name to use in the class definition. Can be the same as that in the ancillary text draw definitions
+        as they are deconflicted within the macros
+    bone - The bone the text should be transformed by. Use "Null" when not required
+    startX, startY - Position of the text
+    config - Configuration describing the content, like <MPD_TEXT_STATIC>, <MPD_TEXT_USER> and <MPD_TEXT_USER_SCALE>
+*/
 #define MPD_TEXT_BONE_R(name, bone, startX, startY, config) MPD_TEXT(name, bone, startX, startY, align = right; config)
 #define MPD_TEXT_BONE_C(name, bone, startX, startY, config) MPD_TEXT(name, bone, startX, startY, align = center; config)
 #define MPD_TEXT_BONE_L(name, bone, startX, startY, config) MPD_TEXT(name, bone, startX, startY, align = left; config)
 
+/* Macro: MPD_TEXT_SMALL_x
+    Draws small text, with the text content defined by config.
+
+Parameters:
+    name - Unique name to use in the class definition. Can be the same as that in the ancillary text draw definitions
+        as they are deconflicted within the macros
+    startX, startY - Position of the text
+    config - Configuration describing the content, like <MPD_TEXT_STATIC>, <MPD_TEXT_USER> and <MPD_TEXT_USER_SCALE>
+*/
 #define MPD_TEXT_SMALL_R(name, startX, startY, config) MPD_TEXT_SMALL(name, startX, startY, align = right; config)
 #define MPD_TEXT_SMALL_C(name, startX, startY, config) MPD_TEXT_SMALL(name, startX, startY, align = center; config)
 #define MPD_TEXT_SMALL_L(name, startX, startY, config) MPD_TEXT_SMALL(name, startX, startY, align = left; config)
 
+/* Macro: MPD_TEXT_x
+    Draws text, with the text content defined by config.
+
+Parameters:
+    name - Unique name to use in the class definition. Can be the same as that in the ancillary text draw definitions
+        as they are deconflicted within the macros
+    startX, startY - Position of the text
+    config - Configuration describing the content, like <MPD_TEXT_STATIC>, <MPD_TEXT_USER> and <MPD_TEXT_USER_SCALE>
+*/
 #define MPD_TEXT_R(name, startX, startY, config) MPD_TEXT(name, Null, startX, startY, align = right; config)
 #define MPD_TEXT_C(name, startX, startY, config) MPD_TEXT(name, Null, startX, startY, align = center; config)
 #define MPD_TEXT_L(name, startX, startY, config) MPD_TEXT(name, Null, startX, startY, align = left; config)
@@ -203,20 +445,16 @@ class Mpd_Arrow_##name##_Triangle { \
 #define MPD_BOX_TALL_C(name, startX, startY, numChars) MPD_BOX_TALL_R(name, (startX - numChars / 2 * MPD_TEXT_WIDTH), startY, numChars)
 #define MPD_BOX_TALL_L(name, startX, startY, numChars) MPD_BOX_TALL_R(name, (startX - numChars * MPD_TEXT_WIDTH), startY, numChars)
 
-#define MPD_BOX_ARROW_R(name, startX, startY, numChars) class Mpd_Box_Arrow_##name##_Line { \
-    type = line; \
-    width = 2; \
-    points[] = { \
-        {{startX - MPD_BOX_PAD, startY - MPD_BOX_PAD - MPD_ARROW_HEIGHT - 0.5 * MPD_ARROW_PAD}, 1}, \
-        {{startX + MPD_BOX_PAD + numChars * MPD_TEXT_WIDTH, startY - MPD_BOX_PAD- MPD_ARROW_HEIGHT - 0.5 * MPD_ARROW_PAD}, 1}, \
-        {{startX + MPD_BOX_PAD + numChars * MPD_TEXT_WIDTH, startY + MPD_TEXT_HEIGHT + MPD_BOX_PAD}, 1}, \
-        {{startX - MPD_BOX_PAD, startY + MPD_TEXT_HEIGHT + MPD_BOX_PAD}, 1}, \
-        {{startX - MPD_BOX_PAD, startY - MPD_BOX_PAD - MPD_ARROW_HEIGHT - 0.5 * MPD_ARROW_PAD}, 1}}; \
-};
+/* Macro: MPD_TEXT_OCCLUDER_x
+    type="polygon" polygon that is the same size as text. Used to occlude items behind text
 
-#define MPD_BOX_ARROW_C(name, startX, startY, numChars) MPD_BOX_ARROW_R(name, (startX - numChars / 2 * MPD_TEXT_WIDTH), startY, numChars)
-#define MPD_BOX_ARROW_L(name, startX, startY, numChars) MPD_BOX_ARROW_R(name, (startX - numChars * MPD_TEXT_WIDTH), startY, numChars)
-
+Parameters:
+    name - Unique name to use in the class definition. Can be the same as that in the ancillary text draw definitions
+        as they are deconflicted within the macros
+    bone - The bone the text should be transformed by. Use "Null" when not required.
+    startX, startY - Position of the text that this should oclcude for.
+    numChars - Number of characters in the text to occlude for, sets size.
+*/
 #define MPD_TEXT_OCCLUDER_R(bone,startX,startY,numChars) { \
     {bone, {(startX), (startY)}, 1},\
     {bone, {(startX) + (numChars) * MPD_TEXT_WIDTH, (startY)}, 1},\
@@ -227,6 +465,17 @@ class Mpd_Arrow_##name##_Triangle { \
 #define MPD_TEXT_OCCLUDER_C(bone,startX,startY,numChars) MPD_TEXT_OCCLUDER_R(bone,((startX)-(numChars)/2*MPD_TEXT_WIDTH),startY,numChars)
 #define MPD_TEXT_OCCLUDER_L(bone,startX,startY,numChars) MPD_TEXT_OCCLUDER_R(bone,((startX)-(numChars)*MPD_TEXT_WIDTH),startY,numChars)
 
+/* Macros: Barriers
+    MPD_BOX_BAR_B - Draws barrier for text at VAB B[1-6]
+    MPD_BOX_BAR_T - Draws barrier for text at VAB T[1-6]
+    MPD_BOX_BAR_L - Draws barrier for text at VAB L[1-6]
+    MPD_BOX_BAR_R - Draws barrier for text at VAB R[1-6]
+
+Parameters:
+    name - Unique name to use in the class definition. Can be the same as that in the ancillary text draw definitions
+        as they are deconflicted within the macros
+    startX, startY - Position of the text that this should oclcude for.
+*/
 #define MPD_BOX_BAR_B(name, startX, startY) class Mpd_Box_##name##_Barrier { \
     color[] = {0.05,0.25,0,0.5};\
     class Polygons {\
