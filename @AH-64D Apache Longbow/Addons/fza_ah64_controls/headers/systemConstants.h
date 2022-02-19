9519bb8f67cd551cc5ee8e84e0087a91df513c0a
BradMick <bradMick@example.com>
2022-02-19 09:30:13 -0600

    Formatting Fix

--- @AH-64D Apache Longbow/Addons/fza_ah64_controls/headers/systemConstants.h
+++ @AH-64D Apache Longbow/Addons/fza_ah64_controls/headers/systemConstants.h
@@ -1,23 +1,23 @@
-#define SIGHT_FCR 0
-#define SIGHT_HMD 1
+#define SIGHT_FCR  0
+#define SIGHT_HMD  1
 #define SIGHT_TADS 2
-#define SIGHT_FXD 3
+#define SIGHT_FXD  3
 
-#define FCR_MODE_OFF 0
-#define FCR_MODE_ON_SINGLE 1
+#define FCR_MODE_OFF           0
+#define FCR_MODE_ON_SINGLE     1
 #define FCR_MODE_ON_CONTINUOUS 2
 
 #define WAS_WEAPON_NONE 0
-#define WAS_WEAPON_GUN 1
-#define WAS_WEAPON_RKT 2
-#define WAS_WEAPON_MSL 3
+#define WAS_WEAPON_GUN  1
+#define WAS_WEAPON_RKT  2
+#define WAS_WEAPON_MSL  3
 
 //FCR 
-#define FCR_LIMIT_MIN_RANGE 500
+#define FCR_LIMIT_MIN_RANGE              500
 #define FCR_LIMIT_LOAL_LOBL_SWITCH_RANGE 1500
-#define FCR_LIMIT_STATIONARY_RANGE 6000
-#define FCR_LIMIT_MOVING_RANGE 8000
-#define FCR_LIMIT_MOVING_MIN_SPEED_KMH 6
+#define FCR_LIMIT_STATIONARY_RANGE       6000
+#define FCR_LIMIT_MOVING_RANGE           8000
+#define FCR_LIMIT_MOVING_MIN_SPEED_KMH   6
 //--FCR Priority
 #define FCR_TYPE_UNKNOWN    0
 #define FCR_TYPE_FLYER      1
