d2fb774eed8c3df16db4598bbad540b0e1995269
mattysmith22 <mattysmith22@googlemail.com>
2022-05-16 23:37:23 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@f73ac564bff22ee04f9081df3bc3987dee38a898 🚀

--- files/functions/ui/fn_uiShowIntro-sqf-SummaryToolTips.js
+++ files/functions/ui/fn_uiShowIntro-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:functions/ui/fn_uiShowIntro.sqf",{86:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">onLoad event handler for the introduction popup Parameters: _display - display that the dialog is opened up on Returns: Nothing Examples:</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:functions/ui/fn_uiShowIntro.sqf",{88:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">onLoad event handler for the introduction popup Parameters: _display - display that the dialog is opened up on Returns: Nothing Examples:</div></div>"});
