f317b676c517284992eab15ce10101ea273c98cf
BradMick <bradMick@example.com>
2022-08-31 20:27:22 -0500

    Update
    Added Hover Q box

--- @AH-64D Apache Longbow/Addons/fza_ah64_mpd/functions/page/fn_pagePERFDraw.sqf
+++ @AH-64D Apache Longbow/Addons/fza_ah64_mpd/functions/page/fn_pagePERFDraw.sqf
@@ -1,12 +1,16 @@
 #include "\fza_ah64_mpd\headers\mfdConstants.h"
+#define SCALE_METERS_FEET 3.28084
+#define KG_TO_LBS 2.20462
 
 params ["_heli", "_mpdIndex"];
 
 private _baroAlt  = getPosASL _heli # 2 * SCALE_METERS_FEET;
+private _baroAlt     = getPosASL _heli # 2 * SCALE_METERS_FEET;
 
 private _pa  = (round (_baroAlt / 10) * 10) toFixed 0;
 private _fat = (ambientTemperature select 0) toFixed 0;
 private _gwt = ((getMass _heli) * 2.20462) toFixed 0;
+private _gwt = (getMass _heli * KG_TO_LBS) toFixed 0;
 
 hintsilent format ["PA = %1
                    \nFAT = %2
--- @AH-64D Apache Longbow/Addons/fza_ah64_mpd/pages/perf_draw.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_mpd/pages/perf_draw.hpp
@@ -5,11 +5,37 @@
         width = 3;
         points[] = {
             //Left side, vertical
-            {{0.300, 0.982}, 1}, 
-            {{0.300, 0.929}, 1}, {},
+            {{0.270, 0.985}, 1}, 
+            {{0.270, 0.925}, 1}, {},
             //Left side, top horizontal
-            {{0.300, 0.929}, 1}, 
-            {{0.334, 0.929}, 1}, {},
+            {{0.270, 0.925}, 1}, 
+            {{0.355, 0.925}, 1}, {},
+            {{0.270, 0.925}, 1}, 
+            {{0.355, 0.925}, 1}, {},
+            //Right side, top horizontal
+            {{0.550, 0.925}, 1}, 
+            {{0.635, 0.925}, 1}, {},
+            //Right side, vertical
+            {{0.635, 0.925}, 1}, 
+            {{0.635, 0.985}, 1}, {},
+        };
+    };
+
+    class hover_q_box {
+        class hover_q_box_outline {
+            type = line;
+            width = 3;
+            points[] = {
+                MPD_POINTS_BOX(Null, (10*MPD_TEXT_WIDTH) - 0.005, (2.5*MPD_TEXT_HEIGHT), 16.2*MPD_TEXT_WIDTH, 6.2*MPD_TEXT_HEIGHT), {},
+            };
+        };
+
+        class hover_q_text {
+             MPD_TEXT_R(HOV_Q,   MPD_TEXT_WIDTH * 10,  MPD_TEXT_HEIGHT * 2.5, MPD_TEXT_STATIC(  "HOVER Q"))
+             MPD_TEXT_R(IGE_OGE, MPD_TEXT_WIDTH * 10,  MPD_TEXT_HEIGHT * 3.8, MPD_TEXT_STATIC("         IGE OGE"))
+             MPD_TEXT_R(REQ,     MPD_TEXT_WIDTH * 10, MPD_TEXT_HEIGHT * 5.1, MPD_TEXT_STATIC( "REQUIRED 067 083"))
+             MPD_TEXT_R(GO_NOGO, MPD_TEXT_WIDTH * 10, MPD_TEXT_HEIGHT * 6.4, MPD_TEXT_STATIC( "GO-NO/GO 100 082"))
+             MPD_TEXT_R(IND,     MPD_TEXT_WIDTH * 10, MPD_TEXT_HEIGHT * 7.7, MPD_TEXT_STATIC( "INDICATED  019"))
         };
     };
 
@@ -24,7 +50,8 @@
         MPD_ARROW_C(FUEL, MPD_POS_BUTTON_TB_3_X, MPD_POS_BUTTON_T_Y, 4)
         MPD_TEXT_C(FUEL,  MPD_POS_BUTTON_TB_3_X, MPD_POS_BUTTON_T_Y, MPD_TEXT_STATIC("FUEL"))
 
-        MPD_ARROW_C(UTIL, MPD_POS_BUTTON_TB_6_X, MPD_POS_BUTTON_T_Y, 5)
+        //MPD_ARROW_C(UTIL, MPD_POS_BUTTON_TB_6_X, MPD_POS_BUTTON_T_Y, 5)
+        MPD_BOX_BAR_T(UTIL, MPD_POS_BUTTON_TB_6_X, MPD_POS_BUTTON_T_Y)
         MPD_TEXT_C(UTIL,  MPD_POS_BUTTON_TB_6_X, MPD_POS_BUTTON_T_Y, MPD_TEXT_STATIC("UTIL"))
 
         ///////////////// L ///////////////
@@ -45,14 +72,20 @@
 
         MPD_BOX_C(PERF,  MPD_POS_BUTTON_TB_2_X, MPD_POS_BUTTON_B_Y, 3)
         MPD_TEXT_C(CUR,  MPD_POS_BUTTON_TB_2_X, MPD_POS_BUTTON_B_Y, MPD_TEXT_STATIC("CUR"))
+        
+        MPD_BOX_BAR_B(MAX, MPD_POS_BUTTON_TB_3_X, MPD_POS_BUTTON_B_Y)
         MPD_TEXT_C(MAX,  MPD_POS_BUTTON_TB_3_X, MPD_POS_BUTTON_B_Y, MPD_TEXT_STATIC("MAX"))
+        
+        MPD_BOX_BAR_B(PLAN, MPD_POS_BUTTON_TB_4_X, MPD_POS_BUTTON_B_Y)
         MPD_TEXT_C(PLAN, MPD_POS_BUTTON_TB_4_X, MPD_POS_BUTTON_B_Y, MPD_TEXT_STATIC("PLAN"))
 
         MPD_TEXT_C(MODE, MPD_POS_BUTTON_TB_3_X, MPD_POS_BUTTON_B_Y - 1.1*MPD_TEXT_HEIGHT, MPD_TEXT_STATIC("PERF MODE"))
 
         MPD_ARROW_C(HIT, MPD_POS_BUTTON_TB_5_X, MPD_POS_BUTTON_B_Y, 3)
+        MPD_BOX_BAR_B(HIT, MPD_POS_BUTTON_TB_5_X, MPD_POS_BUTTON_B_Y)
         MPD_TEXT_C(HIT,  MPD_POS_BUTTON_TB_5_X, MPD_POS_BUTTON_B_Y, MPD_TEXT_STATIC("HIT"))
 
+        MPD_BOX_BAR_B(WT, MPD_POS_BUTTON_TB_6_X, MPD_POS_BUTTON_B_Y)
         MPD_TEXT_C(WT,   MPD_POS_BUTTON_TB_6_X, MPD_POS_BUTTON_B_Y, MPD_TEXT_STATIC("WT"))
     };
 };
