1e9152ba4ed45311854298e1619471015305b60a
Rosd6(Dryden) <Doolieollie@gmail.com>
2022-10-04 23:53:56 +0100

    Init

---
+++ @AH-64D Apache Longbow/Addons/fza_ah64_misc/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches
+{
+	class fza_ah64_Misc
+	{
+        units[] = {"fza_ah64_FireControlRadar", "fza_ah64_IAFS", "fza_ah64_1200Magazine"};
+		author ="Franze, Nodunit, Voodooflies, Keplager, mattysmith22, BradMick, Rosd6(Dryden) & Community";
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"fza_ah64_controls"};
+    };
+};
+
+#include "Config\cfgvehicles.hpp"
