78a3ccf1174e81b03089db9d3eda0b0698654047
BradMick <bradMick@example.com>
2022-10-18 17:36:51 -0500

    Initial Commit
    + Broke out CfgVehicles into individual component parts for readability and ease of use.

---
+++ @AH-64D Apache Longbow/Addons/fza_ah64_controls/config/cfgVehicles/turrets.hpp
@@ -0,0 +1,193 @@
+class Turrets
+{
+    class MainTurret: NewTurret
+    {
+        gunnerAction = "fza_ah64_copilot";
+        gunnerGetInAction = "pilot_Heli_Light_02_Enter";
+        gunnerGetOutAction = "GetOutHigh";
+        preciseGetInOut = 1;
+        hideWeaponsGunner = false;
+        primary = 1;
+        primaryGunner = 1;
+        stabilizedInAxes = 3;
+        weapons[] = {"fza_ma_safe", "fza_gun_safe", "fza_rkt_safe", "fza_msl_safe", "Laserdesignator_mounted", "fza_burstlimiter","fza_m230"};
+        magazines[] = {"fza_safe", "LaserBatteries", "fza_m230_300"};
+        memoryPointsGetInGunner = "pos gunner";
+        memoryPointsGetInGunnerDir = "pos gunner dir";
+        memoryPointGun = "laserBegin";
+        memoryPointGunnerOptics = "gunnerview";
+        body = "tads_tur";
+        gun = "tads";
+        animationsourcebody = "tads_tur";
+        animationsourcegun = "tads";
+        gunBeg = "laserBegin";
+        gunEnd = "laserEnd";
+        gunnerOpticsModel = "";
+        gunnerOpticsColor[] = {1,1,1,1};
+        minElev = -60;
+        maxElev = 30;
+        initElev= 0;
+        maxXRotSpeed = 1;	// yawing speed
+        maxYRotSpeed = 1;	// pitching speed
+        maxMouseXRotSpeed= 0.5;
+        maxMouseYRotSpeed= 0.5;
+        minTurn = -120;
+        maxTurn = 120;
+        initTurn = 0;
+        minGunElev = -60;
+        maxGunElev = 30;
+        minGunTurn = -120;
+        maxGunTurn = 120;
+        minGunTurnAI = -90;
+        maxGunTurnAI = 90;
+        commanding = -1;
+        gunnerForceOptics = 0;
+        startEngine=0;
+        outGunnerMayFire = 1;
+        turretinfotype = "RscUnitInfoNoHUD";
+        turretFollowFreeLook = 0;
+        discreteDistance[]={100,200,300,400,500,600,700,800,1000,1200,1500,1800,2100,2500,2800,3300};
+        discreteDistanceInitIndex=5;
+        isCopilot = 1;
+        usePiP=1;
+        class Reflectors
+        {
+            class cabin
+            {
+                color[]={0.306, 0.878, 0.349};
+                ambient[] = {0.306, 0.878, 0.349};
+                intensity = 25;
+                size = 1;
+                innerAngle = 30;
+                outerAngle = 150;
+                coneFadeCoef = 1;
+                position = "plt_floodlamps";
+                direction = "plt_memflood";
+                hitpoint = "plt_floodlamps";
+                selection = "plt_floodlamps";
+                useFlare = 0;
+                flareSize = 0;
+                flareMaxDistance = 0;
+                dayLight = 1;
+                blinking = 0;
+                class Attenuation
+                {
+                    start           = 0;
+                    constant        = 0;
+                    linear          = 1;
+                    quadratic       = 2;
+                    hardLimitStart  = 0.65;
+                    hardLimitEnd    = 1.9;
+                };
+            };
+            class cargo_light_1: cabin
+            {
+                position = "cpg_flood";
+                direction = "cpg_memflood";
+                hitpoint = "cpg_flood";
+                selection = "cpg_flood";
+            };
+        };
+        class HitPoints
+        {
+            class HitTurret
+            {
+                armor = 0.9;
+                material = 51;
+                name = "tads_tur";
+                visual = "skin_tads1";
+                passThrough = 1;
+            };
+            class HitGun
+            {
+                armor = 1.3;
+                material = 52;
+                name = "otochlaven";
+                visual = "skin_otochlaven";
+                passThrough = 1;
+            };
+        };
+        class OpticsIn
+        {
+            class Wide
+            {
+                gunneropticsmodel = "\fza_ah64_us\fza_ah64_optics_empty";
+                directionStabilized = 1;
+                initanglex = 0;
+                initangley = 0;
+                initfov = 0.466;
+                maxanglex = 30;
+                maxangley = 120;
+                maxfov = 0.466;
+                minanglex = -60;
+                minangley = -120;
+                minfov = 0.466;
+                opticsdisplayname = "W";
+                thermalmode[] = {0,1};
+                visionmode[] = {"Normal","Ti"};
+            };
+            class Medium: Wide
+            {
+                gunneropticsmodel = "\fza_ah64_us\fza_ah64_optics_empty";
+                initfov = 0.09;
+                maxfov = 0.09;
+                minfov = 0.09;
+                opticsdisplayname = "M";
+            };
+            class Narrow: Wide
+            {
+                gunneropticsmodel = "\fza_ah64_us\fza_ah64_optics_empty";
+                initfov = 0.01;
+                maxfov = 0.01;
+                minfov = 0.01;
+                opticsdisplayname = "N";
+            };
+        };
+        class OpticsOut
+        {
+            class Monocular //FOV CPG
+            {
+                gunneropticseffect[] = {};
+                gunneropticsmodel = "";
+                initfov = 0.7;
+                initanglex = 0;
+                initangley = 0;
+                minfov = 0.4;
+                maxfov = 0.7;
+                minanglex = -60;
+                maxanglex = 30;
+                minangley = -120;
+                maxangley = 120;
+            };
+        };
+        class Components
+        {
+            class VehicleSystemsDisplayManagerComponentLeft
+            {
+                componentType = "VehicleSystemsDisplayManager";
+                x = "(profilenamespace getvariable [""IGUI_GRID_CUSTOMINFOLEFT_X"",	(safezoneX + 0.5 * (((safezoneW / safezoneH) min 1.2) / 40))])";
+                y = "(profilenamespace getvariable [""IGUI_GRID_CUSTOMINFOLEFT_Y"",	(safezoneY + safezoneH - 21 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25))])";
+                left = 1;
+                defaultDisplay = "CrewDisplay";
+                class Components
+                {
+                    class EmptyDisplay		// Empty display - hide panel
+                    {
+                        componentType = "EmptyDisplayComponent";
+                    };
+                    class MinimapDisplay	// GPS
+                    {
+                        componentType = "MinimapDisplayComponent";
+                        resource = "RscCustomInfoMiniMap";
+                    };
+                };
+            };
+            class VehicleSystemsDisplayManagerComponentRight : VehicleSystemsDisplayManagerComponentLeft {
+                left = 0;
+                right = 1;
+                x = "(profilenamespace getvariable [""IGUI_GRID_CUSTOMINFORIGHT_X"",((safezoneX + safezoneW) - ((10 * (((safezoneW / safezoneH) min 1.2) / 40)) + 0.5 * (((safezoneW / safezoneH) min 1.2) / 40)))])";
+                y = "(profilenamespace getvariable [""IGUI_GRID_CUSTOMINFORIGHT_Y"",(safezoneY + safezoneH - 21 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25))])";
+            };
+        };
+    };
+};
---
+++ @AH-64D Apache Longbow/Addons/fza_ah64_systems/cfgVehicles/hitPoints.hpp
@@ -0,0 +1,344 @@
+#define HIT_RADIUS_01 0.14
+#define HIT_RADIUS_02 0.05
+#define ARMOR_SCALAR  0.067
+
+class HitPoints		
+{		
+    class hit_apu	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.30;
+        name = "hit_apu";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_drives_intermediategearbox	
+    {	
+        armor = 1.62 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.34;
+        name = "hit_drives_intermediategearbox";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_drives_noseGearbox1	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.24;
+        name = "hit_drives_noseGearbox1";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_drives_noseGearbox2	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.24;
+        name = "hit_drives_noseGearbox2";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_drives_tailrotorgearbox	
+    {	
+        armor = 1.62 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.34;
+        name = "hit_drives_tailrotorgearbox";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_drives_transmission	
+    {	
+        armor = 2.88 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.30;
+        name = "hit_drives_transmission";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_elec_battery	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.24;
+        name = "hit_elec_battery";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_elec_generator1	
+    {	
+        armor = 0.72 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.09;
+        name = "hit_elec_generator1";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_elec_generator2	
+    {	
+        armor = 0.72 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.09;
+        name = "hit_elec_generator2";
+        material = 51;
+        passThrough = 0;
+    };
+    class hit_elec_rectifier1
+    {
+        armor = 0.72 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.10;
+        name = "hit_elec_rectifier1";
+        material = 51;
+        passThrough = 0;
+    };
+    class hit_elec_rectifier2
+    {
+        armor = 0.72 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.10;
+        name = "hit_elec_rectifier2";
+        material = 51;
+        passThrough = 0;
+    };
+    class hitengine1	
+    {	
+        armor = 1.80 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.30;
+        name = "hitengine1";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hitengine2	
+    {	
+        armor = 1.80 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.30;
+        name = "hitengine2";
+        material = 51;
+        passThrough = 0;
+    };
+    class hit_fuel_aft {
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.15;
+        name = "hit_fuel_aft";
+        material = 51;
+        passThrough = 0;
+    };
+    class hit_fuel_forward {
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.15;
+        name = "hit_fuel_forward";
+        material = 51;
+        passThrough = 0;
+    };
+    class hit_msnEquip_magandrobbie {
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.15;
+        name = "hit_msnEquip_magandrobbie";
+        material = 51;
+        passThrough = 0;
+    };
+    class hit_hyd_prireservoir	
+    {	
+        armor = 0.72 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.20;
+        name = "hit_hyd_prireservoir";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_hyd_priPump	
+    {	
+        armor = 0.90 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.15;
+        name = "hit_hyd_priPump";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_hyd_utilreservoir	
+    {	
+        armor = 0.72 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.20;
+        name = "hit_hyd_utilreservoir";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_hyd_utilPump	
+    {	
+        armor = 0.90 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_02;
+        minimalHit = 0.1;
+        explosionShielding = 0.15;
+        name = "hit_hyd_utilPump";
+        material = 51;
+        passThrough = 0;
+    };
+    class hithrotor
+    {
+        armor = 5.76 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hithrotor";
+        material = 51;
+        passThrough = 0;
+    };
+    class hit_msnEquip_gun_turret	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_gun_turret";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_pnvs_flir	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_pnvs_flir";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_pnvs_turret	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_pnvs_turret";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_pylon1	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_pylon1";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_pylon2	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_pylon2";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_pylon3	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_pylon3";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_pylon4	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_pylon4";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_tads_dtv	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_tads_dtv";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_tads_flir	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_tads_flir";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_tads_turret	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_tads_turret";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_msnEquip_fcr	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_msnEquip_fcr";
+        material = 51;
+        passThrough = 0;
+    };	
+    class hit_stabilator	
+    {	
+        armor = 1.44 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hit_stabilator";
+        material = 51;
+        passThrough = 0;
+    };
+    class hitvrotor
+    {	
+        armor = 5.76 * ARMOR_SCALAR;
+        radius = HIT_RADIUS_01;
+        minimalHit = 0.1;
+        explosionShielding = 0.80;
+        name = "hitvrotor";
+        material = 51;
+        passThrough = 0;
+    };		
+};		
