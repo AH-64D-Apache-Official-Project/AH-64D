cc5ad53f9b7c2c04c88f38f21ab7e9121c5e4410
mattysmith22 <mattysmith22@googlemail.com>
2023-05-01 14:28:38 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@50ecfa95752c5c71d003a142a05cd6422ce0e965 🚀

--- files/core/fn_coreGetObjectsLookedAt-sqf-SummaryToolTips.js
+++ files/core/fn_coreGetObjectsLookedAt-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:core/fn_coreGetObjectsLookedAt.sqf",{200:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Retrieves information about each object that is looked at by the user.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:core/fn_coreGetObjectsLookedAt.sqf",{198:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Retrieves information about each object that is looked at by the user.</div></div>"});
--- files/engine/fn_engineSetPosition-sqf-SummaryToolTips.js
+++ files/engine/fn_engineSetPosition-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:engine/fn_engineSetPosition.sqf",{100:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Handles the controls for each individual engine</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:engine/fn_engineSetPosition.sqf",{98:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Handles the controls for each individual engine</div></div>"});
