c7940e75e4000cd7026a6086dd30e487d199259e
Matthew Smith <mattysmith22@googlemail.com>
2023-05-06 16:19:57 +0100

    Remove tabs from fza_ah64_audio

--- @AH-64D Apache Longbow/Addons/fza_ah64_audio/cfgFunctions.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_audio/cfgFunctions.hpp
@@ -5,19 +5,19 @@
 #endif
 class CfgFunctions
 {
-	class fza_ah64_project_audio
-	{
-		tag = "FZA_audio";
-		class functions {
-			file = "\fza_ah64_audio\functions";
-			class addAdvisoryMessage {R;};
-			class addASEMessage {R;};
-			class addCaution {R;};
-			class addWarning {R;};
-			class audioinit {R;};
-			class getin {R;};
-			class playaudio {R;};
-			class audiohandler {R;};
+    class fza_ah64_project_audio
+    {
+        tag = "FZA_audio";
+        class functions {
+            file = "\fza_ah64_audio\functions";
+            class addAdvisoryMessage {R;};
+            class addASEMessage {R;};
+            class addCaution {R;};
+            class addWarning {R;};
+            class audioinit {R;};
+            class getin {R;};
+            class playaudio {R;};
+            class audiohandler {R;};
         };
     };
 };
--- @AH-64D Apache Longbow/Addons/fza_ah64_audio/config.cpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_audio/config.cpp
@@ -1,12 +1,12 @@
 class CfgPatches
 {
-	class fza_ah64_audio
-	{
+    class fza_ah64_audio
+    {
         units[] = {};
-		author = "Franze, Nodunit, Voodooflies, Keplager, mattysmith22, BradMick, Rosd6(Dryden) & Community";
-		weapons[] = {};
-		requiredVersion = 1.0;
-		requiredAddons[] = {};
+        author = "Franze, Nodunit, Voodooflies, Keplager, mattysmith22, BradMick, Rosd6(Dryden) & Community";
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {};
     };
 };
 
