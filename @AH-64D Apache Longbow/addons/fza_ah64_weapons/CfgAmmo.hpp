a1db3bdf831b6c536547bfa7c3279b5f40f1a7ee
Rosd6(Dryden) <Doolieollie@gmail.com>
2023-05-06 14:10:13 +0100

    Move Files

--- @AH-64D Apache Longbow/addons/fza_ah64_weapons/CfgAmmo.hpp
+++ @AH-64D Apache Longbow/addons/fza_ah64_weapons/CfgAmmo.hpp
@@ -1,17 +1,6 @@
 class CfgAmmo
 {
 	class Default{};
-	class B_30mm_HE;
-	class Bulletbase;
-	class MissileBase;
-	class Missile_AA_04_F;
-	class M_Scalpel_AT : MissileBase {
-		class Components;
-		class EventHandlers;
-	};
-	class FlareCore;
-	class B_9x21_Ball;
-	class ammo_Penetrator_Base;
 	class fza_master: Default
 	{
 		airLock						=0;
@@ -21,88 +10,5 @@ class CfgAmmo
 		artilleryLock				=0;
 		weaponLockSystem			=0;
 		lockType					=2;
-	};
-	
-	///////////////////////////////////////////////////////////////////////
-	//////////////////////////////STINGERS/////////////////////////////////
-	///////////////////////////////////////////////////////////////////////	
-
-	class fza_fim92: Missile_AA_04_F
-	{
-		model = "\fza_ah64_us\fza_fim92";
-		proxyShape = "\fza_ah64_us\fza_fim92";
-		cost = 1000;
-		hit = 100;
-		indirectHit = 75;
-		indirectHitRange = 9;
-		proximityExplosionDistance = 15;
-		warheadName = "HE";
-		airLock = 2;
-		irLock = 1;
-		laserLock = 0;
-		nvLock = 0;
-		missileLockCone = 45;
-		missileKeepLockedCone = 60;
-		missileLockMaxDistance = 4500;
-		missileLockMinDistance = 150;
-		missileLockMaxSpeed = 445;
-		weaponLockSystem = "2 + 16";
-		cmImmunity = 0.9;
-		manualControl = 0;
-		maxControlRange = 8000;
-		aiAmmoUsageFlags = 256;
-		initTime = 0;
-		thrust = 220;
-		thrustTime = 5;
-		airFriction = 0.14;
-		sideAirFriction = 0.2;
-		maxSpeed = 2700;
-		maneuvrability = 36;
-		simulationStep = 0.002;
-		fuseDistance = 500;
-		timeToLive = 20;
-		trackLead = 0.9;
-		trackOversteer = 1;
-		craterEffects = "ATRocketCrater";
-		effectsMissileInit = "PylonBackEffects";
-		effectsmissile = "fza_ah64_stingersmoke";
-		explosioneffects = "AAMissileExplosion";
-		muzzleEffect = "fza_ah64_fx_msl_shake";
-		whistleDist = 16;
-		soundsetexplosion[] = {"fza_rocket_explosion_soundset", "fza_rocket_explosion_stereo_soundset", "fza_shell_explosion_reverb_soundset"};		
-		soundsetsoniccrack[] = {"fza_sc_rocket_flyby_soundset"};
-		soundfly[]  = {"fza_ah64_us\audio\Flyby\soundfly1.ogg", 1, 1.500000, 400};
-		class Hiteffects
-		{
-			hitWater = "ImpactEffectsSmall";
-		};
-		class CamShakeExplode
-		{
-			power = 22;
-			duration = 2;
-			frequency = 20;
-			distance = 163.905;
-		};
-		class CamShakeHit
-		{
-			power = 110;
-			duration = 0.6;
-			frequency = 20;
-			distance = 1;
-		};
-		class CamShakeFire
-		{
-			power = 2.9907;
-			duration = 1.8;
-			frequency = 20;
-			distance = 71.5542;
-		};
-		class CamShakePlayerFire
-		{
-			power = 4;
-			duration = 0.1;
-			frequency = 20;
-			distance = 1;
-		};
 	};
 };
--- @AH-64D Apache Longbow/addons/fza_ah64_weapons/CfgMagazines.hpp
+++ @AH-64D Apache Longbow/addons/fza_ah64_weapons/CfgMagazines.hpp
@@ -1,11 +1,6 @@
 class CfgMagazines
 {
-	class CA_Magazine;
 	class VehicleMagazine;
-	class 24Rnd_PG_missiles;
-	class 24Rnd_missiles;
-	class 6Rnd_Missile_AGM_02_F;
-	
 	class fza_safe: VehicleMagazine
 	{
 		scope = 1;
@@ -25,110 +20,5 @@ class CfgMagazines
 		initSpeed=0;
 		tracersEvery=0;
 		nameSound="";
-	};
-	
-	////////////////////////////////////////////////////////////////////////
-	//////////////////////////////FUEL TANKS////////////////////////////////
-	////////////////////////////////////////////////////////////////////////
-
-	class fza_iafs_100: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "IAFS 100gal";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-		weight = 320.6;
-	};
-	class fza_iafs_130: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "IAFS 130gal";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-		weight = 320.6;
-	};
-	class fza_auxtank_230_1: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "Wing Aux 230gal";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-		weight = 698.9858;
-	};
-	class fza_auxtank_230_2: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "Wing Aux 230gal";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-		weight = 698.9858;
-	};
-	class fza_auxtank_230_3: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "Wing Aux 230gal";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-		weight = 698.9858;
-	};
-	class fza_auxtank_230_4: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "Wing Aux 230gal";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-		weight = 698.9858;
-	};
-
-	///////////////////////////////////////////////////////////////////////////
-	///////////////////////////DAMAGE//////////////////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
-	class fza_ah64_rdp_fail: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "RDP FAIL";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-	};
-	class fza_ah64_ldp_fail: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "LDP FAIL";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-	};
-	class fza_ah64_rwp_fail: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "RWP FAIL";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
-	};
-	class fza_ah64_lwp_fail: VehicleMagazine
-	{
-		scope = 2;
-		displayName = "LWP FAIL";
-		ammo = "fza_30x113";
-		count = 1;
-		initSpeed = 850;
-		tracersevery = 0;
 	};
 };
