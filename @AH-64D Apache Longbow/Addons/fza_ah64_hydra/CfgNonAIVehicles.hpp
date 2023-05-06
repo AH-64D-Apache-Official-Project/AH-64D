8c5a3183217cc29a6a3265ad3d927cee1638fd5f
Rosd6(Dryden) <Doolieollie@gmail.com>
2023-05-06 11:28:11 +0100

    Cfg Seperation

---
+++ @AH-64D Apache Longbow/Addons/fza_ah64_hydra/CfgNonAIVehicles.hpp
@@ -0,0 +1,40 @@
+class CfgNonAIVehicles {
+	class ProxyWeapon;
+ 
+	class Proxyfza_hydra_m151: ProxyWeapon
+	{
+		model = "\fza_ah64_us\fza_hydra_m151.p3d";
+		simulation = "maverickweapon";
+	};
+	class Proxyfza_hydra_m229: ProxyWeapon
+	{
+		model = "\fza_ah64_us\fza_hydra_m229.p3d";
+		simulation = "maverickweapon";
+	};
+	class Proxyfza_hydra_m261: ProxyWeapon
+	{
+		model = "\fza_ah64_us\fza_hydra_m261.p3d";
+		simulation = "maverickweapon";
+	};
+	class Proxyfza_hydra_m257: ProxyWeapon
+	{
+		simulation = "maverickweapon";
+	};
+	class Proxyfza_hydra_m255: ProxyWeapon
+	{
+		simulation = "maverickweapon";
+	};
+	
+	class Proxyfza_pod_zoneA: ProxyWeapon {
+		model = \fza_ah64_us\weps\pylons\fza_pod_zoneA.p3d;
+		simulation = "pylonpod";
+	};
+	class Proxyfza_pod_zoneB: ProxyWeapon {
+		model = \fza_ah64_us\weps\pylons\fza_pod_zoneB.p3d;
+		simulation = "pylonpod";
+	};
+	class Proxyfza_pod_zoneE: ProxyWeapon {
+		model = \fza_ah64_us\weps\pylons\fza_pod_zoneE.p3d;
+		simulation = "pylonpod";
+	};
+};
