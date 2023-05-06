b6ef4ebf0f02d5acb4883eada9d31a7361836969
BradMick <16617553+BradMick@users.noreply.github.com>
2023-05-06 13:50:41 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@24d1d116951e59525994f6a178fdbb612659fa3e 🚀

--- files/sensor/fn_targetingsensorCycle-sqf-SummaryToolTips.js
+++ files/sensor/fn_targetingsensorCycle-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:sensor/fn_targetingsensorCycle.sqf",{100:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">cycle the targeting system to the next FCR target</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:sensor/fn_targetingsensorCycle.sqf",{98:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">cycle the targeting system to the next FCR target</div></div>"});
