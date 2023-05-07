ea24551486add239f25d62e33566be814179afee
Matthew Smith <mattysmith22@googlemail.com>
2023-05-08 00:35:43 +0100

    Remove tabs from fza_ah64_ufd

--- @AH-64D Apache Longbow/Addons/fza_ah64_ufd/config.cpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_ufd/config.cpp
@@ -1,12 +1,12 @@
 class CfgPatches
 {
-	class fza_ah64_ufd
-	{
+    class fza_ah64_ufd
+    {
         units[] = {};
-		author ="Franze, Nodunit, Voodooflies, Keplager, mattysmith22, BradMick, Rosd6(Dryden) & Community";
-		weapons[] = {};
-		requiredVersion = 1.0;
-		requiredAddons[] = {"fza_ah64_controls"};
+        author ="Franze, Nodunit, Voodooflies, Keplager, mattysmith22, BradMick, Rosd6(Dryden) & Community";
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"fza_ah64_controls"};
     };
 };
 
