d3a3733979b21358f48d01dcbcc7c0a53edde8d9
Matthew Smith <mattysmith22@googlemail.com>
2023-05-07 23:59:11 +0100

    Remove tabs from fza_ah64_customise (#281)

--- @AH-64D Apache Longbow/Addons/fza_ah64_customise/config.cpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_customise/config.cpp
@@ -1,32 +1,32 @@
 class CfgPatches
 {
-	class fza_ah64_customise
-	{
+    class fza_ah64_customise
+    {
         units[] = {"fza_ah64_FireControlRadar", "fza_ah64_IAFS", "fza_ah64_1200Magazine"};
-		author ="Franze, Nodunit, Voodooflies, Keplager, mattysmith22, BradMick, Rosd6(Dryden) & Community";
-		weapons[] = {};
-		requiredVersion = 1.0;
-		requiredAddons[] = {"fza_ah64_controls"};
+        author ="Franze, Nodunit, Voodooflies, Keplager, mattysmith22, BradMick, Rosd6(Dryden) & Community";
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"fza_ah64_controls"};
     };
 };
 
 class CfgEditorCategories
 {
-	class EdCat_fza_ah64_Parts
-	{
-		displayName = "AH-64 Parts";
-	};
+    class EdCat_fza_ah64_Parts
+    {
+        displayName = "AH-64 Parts";
+    };
 };
 class CfgEditorSubcategories
 {
-	class EdSubcat_fza_ah64_Magazines
-	{
-		displayName = "AH-64 Magazines";
-	};
-	class EdSubcat_fza_ah64_Components
-	{
-		displayName = "AH-64 Components";
-	};
+    class EdSubcat_fza_ah64_Magazines
+    {
+        displayName = "AH-64 Magazines";
+    };
+    class EdSubcat_fza_ah64_Components
+    {
+        displayName = "AH-64 Components";
+    };
 };
 
 #include "Config\cfgVehicles.hpp"
