61ca242a6bfc5b0922736931b98640874ae9dbc7
Matthew Smith <mattysmith22@googlemail.com>
2023-05-08 00:47:40 +0100

    Remove tabs from fza_ah64_weapons

--- @AH-64D Apache Longbow/Addons/fza_ah64_weapons/config.cpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_weapons/config.cpp
@@ -1,12 +1,12 @@
 class CfgPatches
 {
-	class fza_ah64_weapons
-	{
+    class fza_ah64_weapons
+    {
         units[] = {};
-		author = "Rosd6(Dryden)";
-		weapons[] = {};
-		requiredVersion = 1.0;
-		requiredAddons[] = {};
+        author = "Rosd6(Dryden)";
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {};
     };
 };
 #include "\fza_ah64_controls\config\defines.hpp"
