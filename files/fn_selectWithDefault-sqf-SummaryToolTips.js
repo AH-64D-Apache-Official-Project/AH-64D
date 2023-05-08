79111af172fe2c20d9807fb7b7ba49d680ddba59
BradMick <16617553+BradMick@users.noreply.github.com>
2023-05-08 16:40:32 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@2cd3a5c8aac1024e36d1e1edcbd32407c6b233b7 🚀

--- files/fn_selectWithDefault-sqf-SummaryToolTips.js
+++ files/fn_selectWithDefault-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:fn_selectWithDefault.sqf",{210:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Allows for you to run a select operation while providing a default for if the index is out of range</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:fn_selectWithDefault.sqf",{212:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Allows for you to run a select operation while providing a default for if the index is out of range</div></div>"});
