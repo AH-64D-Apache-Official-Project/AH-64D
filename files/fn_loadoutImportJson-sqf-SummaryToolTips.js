f433f9fa8f033a3cc44715efeef6dfd295a60efd
mattysmith22 <mattysmith22@googlemail.com>
2023-05-08 15:57:08 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@ef928848fdd2a650fb52a9e9f2330dc67e7169fe 🚀

--- files/fn_loadoutImportJson-sqf-SummaryToolTips.js
+++ files/fn_loadoutImportJson-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:fn_loadoutImportJson.sqf",{194:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Import of website heicopter configuration and apply it to the helicopter</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:fn_loadoutImportJson.sqf",{192:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Import of website heicopter configuration and apply it to the helicopter</div></div>"});
