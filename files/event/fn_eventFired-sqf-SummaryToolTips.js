134173da0ec71e417b13433f7050d7052efcee5a
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2023-05-30 17:37:56 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@8731bebefc08cec9696df8039d2bd2f6b526dae3 🚀

--- files/event/fn_eventFired-sqf-SummaryToolTips.js
+++ files/event/fn_eventFired-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:event/fn_eventFired.sqf",{118:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Event handler that handles any ammunition fired by the apache</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:event/fn_eventFired.sqf",{116:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Event handler that handles any ammunition fired by the apache</div></div>"});
