8528d4a46dbdbf7d5bb7a0247f829cd8ece3319c
BradMick <16617553+BradMick@users.noreply.github.com>
2023-06-19 11:21:38 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@194962fc95c229c2d04491ea8b5271619621c1c6 🚀

--- files/light/fn_lightSetCockpitLight-sqf-SummaryToolTips.js
+++ files/light/fn_lightSetCockpitLight-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:light/fn_lightSetCockpitLight.sqf",{124:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Sets the cockpit light to the correct state</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:light/fn_lightSetCockpitLight.sqf",{126:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Sets the cockpit light to the correct state</div></div>"});
