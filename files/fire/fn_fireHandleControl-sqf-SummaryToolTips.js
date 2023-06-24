38f7a473257cd3efd038f5f4e81da5e5b1605e72
BradMick <16617553+BradMick@users.noreply.github.com>
2023-06-24 11:55:21 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@7e692489cdca240bbf3cced4a4f091384b985498 🚀

--- files/fire/fn_fireHandleControl-sqf-SummaryToolTips.js
+++ files/fire/fn_fireHandleControl-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:fire/fn_fireHandleControl.sqf",{154:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Handles any fire-related cockpit controls.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:fire/fn_fireHandleControl.sqf",{156:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Handles any fire-related cockpit controls.</div></div>"});
