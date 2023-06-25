9bf9b4bdbfe274d1104f54452b7327ac5d7e801e
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2023-06-25 16:08:14 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@b0b9d1bbf7d357c60f305b1dd028c6418bcade29 🚀

--- files/targeting/fn_targetingAcqVec-sqf-SummaryToolTips.js
+++ files/targeting/fn_targetingAcqVec-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:targeting/fn_targetingAcqVec.sqf",{136:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Get a vector from the aircraft to the acquisition source point (in world space)</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:targeting/fn_targetingAcqVec.sqf",{134:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Get a vector from the aircraft to the acquisition source point (in world space)</div></div>"});
