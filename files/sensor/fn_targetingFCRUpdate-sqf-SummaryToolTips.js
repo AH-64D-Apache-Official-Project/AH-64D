bd3cefe8c448f54efdf7bbdf8dc259d7a7969f11
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2023-07-05 18:22:12 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@68a46951f3f2f3bf46f7132c215d119c7953d188 🚀

---
+++ files/sensor/fn_targetingFCRUpdate-sqf-SummaryToolTips.js
@@ -0,0 +1,1 @@
+﻿NDSummary.OnToolTipsLoaded("File:sensor/fn_targetingFCRUpdate.sqf",{198:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">cycle the targeting system to the next FCR target</div></div>"});
