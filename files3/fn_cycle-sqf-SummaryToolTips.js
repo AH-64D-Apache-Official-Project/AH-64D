8262c15abf498e34dcb2e51b06340943feb9c491
BradMick <16617553+BradMick@users.noreply.github.com>
2023-07-15 13:39:53 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@bd9038e3a066d24aec3c63e3b05149083577cfda 🚀

--- files3/fn_cycle-sqf-SummaryToolTips.js
+++ files3/fn_cycle-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File3:fn_cycle.sqf",{100:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Generic cycling implementation meant to be used by other functions that must cycle through modes.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File3:fn_cycle.sqf",{12:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Generic cycling implementation meant to be used by other functions that must cycle through modes.</div></div>"});
