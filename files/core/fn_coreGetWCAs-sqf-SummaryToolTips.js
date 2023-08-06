ae4363acf842439515e1c6240cb1502d9d8e6aac
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2023-08-06 17:31:51 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@98326d6eb30bf529bdba589daf8610d85ef3a545 🚀

--- files/core/fn_coreGetWCAs-sqf-SummaryToolTips.js
+++ files/core/fn_coreGetWCAs-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:core/fn_coreGetWCAs.sqf",{172:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Retrieves a list of the warnings, cautions and advisories for the helicopter returned</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:core/fn_coreGetWCAs.sqf",{174:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Retrieves a list of the warnings, cautions and advisories for the helicopter returned</div></div>"});
