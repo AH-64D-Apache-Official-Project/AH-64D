e07721c43a0bc6375c65616595a1f52c43cf96e7
Rosd6(Dryden) <Doolieollie@gmail.com>
2023-08-20 22:02:05 +0100

    RPT error Fixes

--- @AH-64D Apache Longbow/Addons/fza_ah64_controls/config/CfgSettings.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_controls/config/CfgSettings.hpp
@@ -4,7 +4,8 @@ class CfgSettings {
             class fza_ah64_controls {
                 main_addon = "fza_ah64_controls";
                 class Dependencies {
-                    CBA[] = {"cba_main", {3.0.0}, "(true)"};
+                    CBA[] = {"cba_main", {3,15,8}, "(true)"};
+                    ACE[] = {"ace_main", {3,15,2}, "(true)"};
                 };
             };
         };
