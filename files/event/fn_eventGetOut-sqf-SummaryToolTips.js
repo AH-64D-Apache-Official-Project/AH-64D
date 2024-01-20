abb14b5187ae952b5bb09cab58293627aff6918e
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-01-20 23:32:38 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@693770a8cf454488467f49bf10ec611afc399920 🚀

--- files/event/fn_eventGetOut-sqf-SummaryToolTips.js
+++ files/event/fn_eventGetOut-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:event/fn_eventGetOut.sqf",{36:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Event handler that finishes parts of apache logic when someone gets out.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:event/fn_eventGetOut.sqf",{124:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Event handler that finishes parts of apache logic when someone gets out.</div></div>"});
--- files/event/fn_eventInit-sqf-SummaryToolTips.js
+++ files/event/fn_eventInit-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:event/fn_eventInit.sqf",{34:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Master init event handler</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:event/fn_eventInit.sqf",{120:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Master init event handler</div></div>"});
