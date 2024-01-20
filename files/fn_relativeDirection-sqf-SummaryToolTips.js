d17c7866410e0c22cc7384e85fa03ee11189261e
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-01-20 23:44:47 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@a02f2e4ce90ce0dfc956003af339429181704d5f 🚀

--- files/fn_relativeDirection-sqf-SummaryToolTips.js
+++ files/fn_relativeDirection-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:fn_relativeDirection.sqf",{186:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gets the direction of an object as a rotation in degrees off the nose of the aircraft</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:fn_relativeDirection.sqf",{184:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gets the direction of an object as a rotation in degrees off the nose of the aircraft</div></div>"});
