b42175076b01dc5d5f59dff212266530390d9642
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-04-11 22:40:15 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@0b3feecc208e80196745e6cebd1a31e1b054975d 🚀

--- files/laser/fn_laserArm-sqf-SummaryToolTips.js
+++ files/laser/fn_laserArm-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:laser/fn_laserArm.sqf",{122:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Arms the apache\'s LRFD, from either seat. Must be run in scheduled environment (involve sleep)</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:laser/fn_laserArm.sqf",{126:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Arms the apache\'s LRFD, from either seat. Must be run in scheduled environment (involve sleep)</div></div>"});
