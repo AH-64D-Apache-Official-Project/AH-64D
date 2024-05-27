335dea00befeb9940029f0235ef9a49eabcf3390
BradMick <16617553+BradMick@users.noreply.github.com>
2024-05-27 21:17:58 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@998cad7cd7ed7c8af710b5a4e85817422c621c60 🚀

--- files/laser/fn_laserDisarm-sqf-SummaryToolTips.js
+++ files/laser/fn_laserDisarm-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:laser/fn_laserDisarm.sqf",{160:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Disarms the apache\'s LRFD, from either seat.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:laser/fn_laserDisarm.sqf",{158:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Disarms the apache\'s LRFD, from either seat.</div></div>"});
