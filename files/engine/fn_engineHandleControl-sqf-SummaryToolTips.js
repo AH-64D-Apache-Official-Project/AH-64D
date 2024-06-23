1d88a6030fa5edfba85a970c19c51ad1a135494d
mattysmith22 <mattysmith22@googlemail.com>
2024-06-23 07:26:41 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@937a10cae91d492ab213e28cae7860661942930a 🚀

--- files/engine/fn_engineHandleControl-sqf-SummaryToolTips.js
+++ files/engine/fn_engineHandleControl-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:engine/fn_engineHandleControl.sqf",{172:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Handles any engine-related cockpit controls.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:engine/fn_engineHandleControl.sqf",{174:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Handles any engine-related cockpit controls.</div></div>"});
--- files/weapon/fn_weaponTrajectoryChange-sqf-SummaryToolTips.js
+++ files/weapon/fn_weaponTrajectoryChange-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:weapon/fn_weaponTrajectoryChange.sqf",{168:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Cycles through the available hellfire trajectories</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:weapon/fn_weaponTrajectoryChange.sqf",{164:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Cycles through the available hellfire trajectories</div></div>"});
