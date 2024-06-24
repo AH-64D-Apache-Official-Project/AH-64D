546a595f5bd5261d4f9f0aee357d422e55886a23
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-06-24 09:51:03 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@cc0629f49c9390febcdfd0f014d0e03bc3a8dfa0 🚀

--- files/engine/fn_engineHandleControl-sqf-SummaryToolTips.js
+++ files/engine/fn_engineHandleControl-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:engine/fn_engineHandleControl.sqf",{176:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Handles any engine-related cockpit controls.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:engine/fn_engineHandleControl.sqf",{178:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Handles any engine-related cockpit controls.</div></div>"});
