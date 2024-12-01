295eb9dfc1241177b14bd2e5ad8ac3acea21c985
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-01 20:58:31 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@8ebe0a82511f2942ed2dc52be7ee0c48c3babd94 🚀

--- files/anim/fn_animReset-sqf-SummaryToolTips.js
+++ files/anim/fn_animReset-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:anim/fn_animReset.sqf",{170:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Resets an animation to the value on the given variable. Called by onPhaseChanged which is called by bis_fnc_initVehicle.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:anim/fn_animReset.sqf",{172:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Resets an animation to the value on the given variable. Called by onPhaseChanged which is called by bis_fnc_initVehicle.</div></div>"});
