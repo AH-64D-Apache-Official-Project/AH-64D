b0ac3a29dc274261f58d8684592fbcdd0190b087
Snow(Dryden) <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-07 17:02:20 +0000

    Empty Hellfire pylon & Rocket pod

--- @AH-64D Apache Longbow/Addons/fza_ah64_mpd/pylons/hellfire_rf.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_mpd/pylons/hellfire_rf.hpp
@@ -7,38 +7,6 @@ class fza_ah64_hellfire {
     };
     class Draw
     {
-        class NotPresent {
-            condition = "pylonMagazineEmpty";
-            class BackgroundGroup
-            {
-                color[] = { 0.0, 0.0, 0.0, 1.0 };
-                class Background
-                {
-                    type = polygon;
-                    points[] =
-                    {
-                        {
-                            { {-0.0075,-0.062}, 1 },
-                            { { 0.0075,-0.062}, 1 },
-                            { { 0.0075,0.062}, 1 },
-                            { {-0.0075,0.062}, 1 }
-                        };
-                    };
-                };
-            };
-            class Lines {
-                type = line;
-                width=3;
-                points[] = 
-                {
-                    { {-0.0075, -0.062}, 1 },
-                    { { 0.0075, -0.062}, 1 },
-                    { { 0.0075,  0.062}, 1 },
-                    { {-0.0075,  0.062}, 1 },
-                    { {-0.0075, -0.062}, 1 }
-                };
-            };
-        };
         class Present {
             condition = "1-pylonMagazineEmpty";
             class BackgroundGroup
@@ -92,38 +60,6 @@ class fza_ah64_hellfire_selected {
     class Draw
     {
         color[] = { 1.0, 1.0, 1.0, 1.0 };
-        class NotPresent {
-            condition = "pylonMagazineEmpty";
-            class BackgroundGroup
-            {
-                color[] = { 0.0, 0.0, 0.0, 1.0 };
-                class Background
-                {
-                    type = polygon;
-                    points[] =
-                    {
-                        {
-                            { {-0.0075,-0.062}, 1 },
-                            { { 0.0075,-0.062}, 1 },
-                            { { 0.0075,0.062}, 1 },
-                            { {-0.0075,0.062}, 1 }
-                        };
-                    };
-                };
-            };
-            class Lines {
-                type = line;
-                width=3;
-                points[] = 
-                {
-                    { {-0.0075, -0.062}, 1 },
-                    { { 0.0075, -0.062}, 1 },
-                    { { 0.0075,  0.062}, 1 },
-                    { {-0.0075,  0.062}, 1 },
-                    { {-0.0075, -0.062}, 1 }
-                };
-            };
-        };
         class Present {
             condition = "1-pylonMagazineEmpty";
             class BackgroundGroup
@@ -176,22 +112,6 @@ class fza_ah64_hellfire_inverse {
     };
     class Draw
     {
-      class NotPresent {
-            condition = "pylonMagazineEmpty";
-            class Background
-            {
-                type = polygon;
-                points[] =
-                {
-                    {
-                        { {-0.0075,-0.062}, 1 },
-                        { {0.0075,-0.062}, 1 },
-                        { {0.0075,0.062}, 1 },
-                        { {-0.0075,0.062}, 1 }
-                    },
-                };
-            };
-        };
         class Present{
             condition = "1-pylonMagazineEmpty";
             class Background
@@ -247,6 +167,112 @@ class fza_ah64_hellfire_inverse {
                         { {0.02,  -0.038}, 1 }
                     };
                 };
+            };
+        };
+    };
+};
+
+class fza_ah64_M299 {
+    class Bones {
+        class Null {
+            type = fixed;
+            pos[] = {0, 0};
+        };
+    };
+    class Draw
+    {
+        class BackgroundGroup
+        {
+            color[] = { 0.0, 0.0, 0.0, 1.0 };
+            class Background
+            {
+                type = polygon;
+                points[] =
+                {
+                    {
+                        { {-0.0075,-0.062}, 1 },
+                        { { 0.0075,-0.062}, 1 },
+                        { { 0.0075,0.062}, 1 },
+                        { {-0.0075,0.062}, 1 }
+                    };
+                };
+            };
+        };
+        class Lines {
+            type = line;
+            width=3;
+            points[] = 
+            {
+                { {-0.0075, -0.062}, 1 },
+                { { 0.0075, -0.062}, 1 },
+                { { 0.0075,  0.062}, 1 },
+                { {-0.0075,  0.062}, 1 },
+                { {-0.0075, -0.062}, 1 }
+            };
+        };
+    };
+};
+
+class fza_ah64_M299_selected {
+    class Bones {
+        class Null {
+            type = fixed;
+            pos[] = {0, 0};
+        };
+    };
+    class Draw
+    {
+        class BackgroundGroup
+        {
+            class Background
+            {
+                type = polygon;
+                points[] =
+                {
+                    {
+                        { {-0.0075,-0.062}, 1 },
+                        { { 0.0075,-0.062}, 1 },
+                        { { 0.0075,0.062}, 1 },
+                        { {-0.0075,0.062}, 1 }
+                    };
+                };
+            };
+        };
+        class Lines {
+            type = line;
+            width=3;
+            points[] = 
+            {
+                { {-0.0075, -0.062}, 1 },
+                { { 0.0075, -0.062}, 1 },
+                { { 0.0075,  0.062}, 1 },
+                { {-0.0075,  0.062}, 1 },
+                { {-0.0075, -0.062}, 1 }
+            };
+        };
+    };
+};
+
+class fza_ah64_M299_inverse {
+    class Bones {
+        class Null {
+            type = fixed;
+            pos[] = {0, 0};
+        };
+    };
+    class Draw
+    {
+        class Background
+        {
+            type = polygon;
+            points[] =
+            {
+                {
+                    { {-0.0075,-0.062}, 1 },
+                    { {0.0075,-0.062}, 1 },
+                    { {0.0075,0.062}, 1 },
+                    { {-0.0075,0.062}, 1 }
+                },
             };
         };
     };
