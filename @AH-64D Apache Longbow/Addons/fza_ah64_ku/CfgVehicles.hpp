746b8720f0aa56225e54707b44d2c4ef779be019
Snow(Dryden) <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-16 21:46:38 +0000

    Ku base class inheritance correction

--- @AH-64D Apache Longbow/Addons/fza_ah64_ku/CfgVehicles.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_ku/CfgVehicles.hpp
@@ -2,15 +2,11 @@ class CfgVehicles {
     class Helicopter;
     class Helicopter_Base_F : Helicopter {
         class MFD;
+        class Turrets;
         class NewTurret;
-        class Turrets {
-            class MainTurret {
-                class MFD;
-            };
-        };
     };
     class fza_ah64base : Helicopter_Base_F {
-        class MFD : MFD {
+        class MFD {
             class KU {
                 topLeft = "PLT_KU_TL";
                 topRight = "PLT_KU_TR";
@@ -20,7 +16,7 @@ class CfgVehicles {
         };
         class Turrets : Turrets {
             class MainTurret : NewTurret {
-                class MFD : MFD {
+                class MFD {
                     class KU {
                         topLeft = "CPG_KU_TL";
                         topRight = "CPG_KU_TR";
