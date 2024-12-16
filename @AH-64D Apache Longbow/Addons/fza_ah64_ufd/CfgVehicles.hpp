31ab4c13000ee03c6bbc49a90942c14a977e34d0
Snow(Dryden) <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-16 21:52:25 +0000

    UFD Base class inheritance

--- @AH-64D Apache Longbow/Addons/fza_ah64_ufd/CfgVehicles.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_ufd/CfgVehicles.hpp
@@ -2,15 +2,11 @@ class CfgVehicles {
     class Helicopter;
     class Helicopter_Base_F : Helicopter {
         class MFD;
+        class Turrets;
         class NewTurret;
-        class Turrets {
-            class MainTurret {
-                class MFD;
-            };
-        };
     };
     class fza_ah64base : Helicopter_Base_F {
-        class MFD : MFD {
+        class MFD {
             class UFD {
                 topLeft = "PLT_UFD_TL";
                 topRight = "PLT_UFD_TR";
@@ -20,7 +16,7 @@ class CfgVehicles {
         };
         class Turrets : Turrets {
             class MainTurret : NewTurret {
-                class MFD : MFD {
+                class MFD {
                     class UFD {
                         topLeft = "CPG_UFD_TL";
                         topRight = "CPG_UFD_TR";
