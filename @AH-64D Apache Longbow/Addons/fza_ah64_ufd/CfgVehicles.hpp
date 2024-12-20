760850cfdd1b95dc37437738799f2032aed40753
Snow(Dryden) <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-20 18:51:53 +0000

    Config Base class Cleanup (#625)

--- @AH-64D Apache Longbow/Addons/fza_ah64_ufd/CfgVehicles.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_ufd/CfgVehicles.hpp
@@ -3,17 +3,10 @@ class CfgVehicles {
     class Helicopter_Base_F : Helicopter {
         class MFD;
         class Turrets;
+        class NewTurret;
     };
     class fza_ah64base : Helicopter_Base_F {
-        class NewTurret;
-        class Turrets : Turrets {
-            class MainTurret : NewTurret {
-                class MFD;
-            }
-        };
-    };
-    class fza_ah64d_b2e : fza_ah64base {
-        class MFD : MFD {
+        class MFD {
             class UFD {
                 topLeft = "PLT_UFD_TL";
                 topRight = "PLT_UFD_TR";
@@ -23,7 +16,7 @@ class CfgVehicles {
         };
         class Turrets : Turrets {
             class MainTurret : NewTurret {
-                class MFD : MFD {
+                class MFD {
                     class UFD {
                         topLeft = "CPG_UFD_TL";
                         topRight = "CPG_UFD_TR";
