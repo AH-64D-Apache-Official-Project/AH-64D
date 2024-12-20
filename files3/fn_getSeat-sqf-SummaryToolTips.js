cb26b7e712992696f8b359129911087c4c813308
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-20 20:40:34 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@29ea25aec8561d45956d6fd5f59f67071ed00254 🚀

--- files3/fn_getSeat-sqf-SummaryToolTips.js
+++ files3/fn_getSeat-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File3:fn_getSeat.sqf",{110:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gets the seat of a current player Allows for swapping seat position</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File3:fn_getSeat.sqf",{106:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gets the seat of a current player Allows for swapping seat position</div></div>"});
