3507147864ebb90baee1d9387ca2eb073f74ad48
Snow(Dryden) <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-24 19:58:10 +0000

    Invisible Hellfire Bugs (#635)

--- @AH-64D Apache Longbow/Addons/fza_ah64_mpd/CfgMagazines.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_mpd/CfgMagazines.hpp
@@ -6,31 +6,37 @@ class CfgMagazines {
         class mfdElements {
             #include "pylons\hellfire_sal1.hpp"
         };
+        mirrorMissilesIndexes[] = {};
     };
     class fza_agm114k_base : PylonRack_4Rnd_ACE_Hellfire_AGM114K {
         class mfdElements {
             #include "pylons\hellfire_sal2.hpp"
         };
+        mirrorMissilesIndexes[] = {};
     };
     class fza_agm114k2a_base : PylonRack_4Rnd_ACE_Hellfire_AGM114K {
         class mfdElements {
             #include "pylons\hellfire_sal2.hpp"
         };
+        mirrorMissilesIndexes[] = {};
     };
     class fza_agm114l_base : PylonRack_4Rnd_ACE_Hellfire_AGM114K {
         class mfdElements {
             #include "pylons\hellfire_rf.hpp"
         };
+        mirrorMissilesIndexes[] = {};
     };
     class fza_agm114n_base : PylonRack_4Rnd_ACE_Hellfire_AGM114K {
         class mfdElements {
             #include "pylons\hellfire_sal2.hpp"
         };
+        mirrorMissilesIndexes[] = {};
     };
     class fza_agm114_rail : fza_agm114k_ur{
         class mfdElements {
             #include "pylons\hellfire_sal1.hpp"
         };
+        mirrorMissilesIndexes[] = {};
     };
 
     class fza_275_rocket_base : 6Rnd_Missile_AGM_02_F{
