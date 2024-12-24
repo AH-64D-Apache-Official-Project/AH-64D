a4cfe08cc7f55b2356e340481f520e42036e9269
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-24 19:29:13 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@af1f41fffc9a331f79996b81b001d065747410d9 🚀

--- files3/fn_padString-sqf-SummaryToolTips.js
+++ files3/fn_padString-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File3:fn_padString.sqf",{100:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Pads a string to ensure that it is within a fixed size</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File3:fn_padString.sqf",{98:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Pads a string to ensure that it is within a fixed size</div></div>"});
