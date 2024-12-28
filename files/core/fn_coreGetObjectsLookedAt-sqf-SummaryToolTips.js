6ed3169e86b584a2037c3d717c71d4f1e070ea83
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-28 21:47:06 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@9a4f015766ae782bd70dc86141f2c5c54e26612e 🚀

--- files/core/fn_coreGetObjectsLookedAt-sqf-SummaryToolTips.js
+++ files/core/fn_coreGetObjectsLookedAt-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:core/fn_coreGetObjectsLookedAt.sqf",{122:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Retrieves information about each object that is looked at by the user.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:core/fn_coreGetObjectsLookedAt.sqf",{124:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Retrieves information about each object that is looked at by the user.</div></div>"});
