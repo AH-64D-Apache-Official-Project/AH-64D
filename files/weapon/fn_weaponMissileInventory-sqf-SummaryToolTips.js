f9e6ea229b324df009a021ec03d6297bbd0d1c80
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2024-12-28 18:22:17 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@86c2c42d4a27823833781d1d42296d77a281e284 🚀

--- files/weapon/fn_weaponMissileInventory-sqf-SummaryToolTips.js
+++ files/weapon/fn_weaponMissileInventory-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:weapon/fn_weaponMissileInventory.sqf",{152:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gathers missile pylon information information.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:weapon/fn_weaponMissileInventory.sqf",{156:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gathers missile pylon information information.</div></div>"});
