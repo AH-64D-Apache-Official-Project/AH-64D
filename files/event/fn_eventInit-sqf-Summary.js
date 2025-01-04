0bc4daa68b8ceddd75644ed5777b103f0de8e799
BradMick <16617553+BradMick@users.noreply.github.com>
2025-01-04 20:51:45 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@552ef07c98e83df145cae0ccfc1916d4d7a4ba9c 🚀

--- files/event/fn_eventInit-sqf-Summary.js
+++ files/event/fn_eventInit-sqf-Summary.js
@@ -1,1 +1,1 @@
-﻿NDFramePage.OnPageTitleLoaded("File:event/fn_eventInit.sqf","fn_eventInit.sqf");NDSummary.OnSummaryLoaded("File:event/fn_eventInit.sqf",[["SQF","SQF"]],[["Functions","Function"],["Groups","Group"]],[[169,0,1,"Functions","Functions"],[170,0,0,"fza_fnc_eventInit","fza_fnc_eventInit"]]);
+﻿NDFramePage.OnPageTitleLoaded("File:event/fn_eventInit.sqf","fn_eventInit.sqf");NDSummary.OnSummaryLoaded("File:event/fn_eventInit.sqf",[["SQF","SQF"]],[["Functions","Function"],["Groups","Group"]],[[167,0,1,"Functions","Functions"],[168,0,0,"fza_fnc_eventInit","fza_fnc_eventInit"]]);
