a874cdc7b69d4d7d940e91afa92b1c38dbe47b3c
Ollieollieolllie <60583203+Ollieollieolllie@users.noreply.github.com>
2025-04-07 20:01:44 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@cc356f38cd7dab230ea5d44231149cd5d008483d 🚀

--- files/event/fn_eventGetOut-sqf-SummaryToolTips.js
+++ files/event/fn_eventGetOut-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:event/fn_eventGetOut.sqf",{126:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Event handler that finishes parts of apache logic when someone gets out.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:event/fn_eventGetOut.sqf",{158:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Event handler that finishes parts of apache logic when someone gets out.</div></div>"});
--- files/weapon/fn_weaponMissileInventory-sqf-SummaryToolTips.js
+++ files/weapon/fn_weaponMissileInventory-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File:weapon/fn_weaponMissileInventory.sqf",{172:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gathers missile pylon information information.</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File:weapon/fn_weaponMissileInventory.sqf",{186:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gathers missile pylon information information.</div></div>"});
