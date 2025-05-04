d43661a2557b1318e2d79ec1f6fec2b3d6e3d7c4
BradMick <16617553+BradMick@users.noreply.github.com>
2025-05-04 12:50:53 +0000

    Deploying to gh-pages from @ AH-64D-Apache-Official-Project/AH-64D@d61e5d03f05d6c45c8259bc4edcc2eb989aa7287 🚀

--- files/fx/fn_fxMuzzle-sqf-Summary.js
+++ files/fx/fn_fxMuzzle-sqf-Summary.js
@@ -1,1 +1,1 @@
-﻿NDFramePage.OnPageTitleLoaded("File:fx/fn_fxMuzzle.sqf","fn_fxMuzzle.sqf");NDSummary.OnSummaryLoaded("File:fx/fn_fxMuzzle.sqf",[["SQF","SQF"]],[["Functions","Function"],["Groups","Group"]],[[181,0,1,"Functions","Functions"],[182,0,0,"fza_fnc_fxMuzzle","fza_fnc_fxMuzzle"]]);
+﻿NDFramePage.OnPageTitleLoaded("File:fx/fn_fxMuzzle.sqf","fn_fxMuzzle.sqf");NDSummary.OnSummaryLoaded("File:fx/fn_fxMuzzle.sqf",[["SQF","SQF"]],[["Functions","Function"],["Groups","Group"]],[[179,0,1,"Functions","Functions"],[180,0,0,"fza_fnc_fxMuzzle","fza_fnc_fxMuzzle"]]);
--- files3/fn_getSeat-sqf-SummaryToolTips.js
+++ files3/fn_getSeat-sqf-SummaryToolTips.js
@@ -1,1 +1,1 @@
-﻿NDSummary.OnToolTipsLoaded("File3:fn_getSeat.sqf",{116:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gets the seat of a current player Allows for swapping seat position</div></div>"});
+﻿NDSummary.OnToolTipsLoaded("File3:fn_getSeat.sqf",{28:"<div class=\"NDToolTip TFunction LSQF\"><div class=\"TTSummary\">Gets the seat of a current player Allows for swapping seat position</div></div>"});
