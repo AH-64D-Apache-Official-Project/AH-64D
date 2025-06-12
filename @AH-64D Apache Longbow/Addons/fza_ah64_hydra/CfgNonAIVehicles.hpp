1de9f6524e54a1a159a92ec96bda39833febd980
Snow(Dryden) <60583203+Ollieollieolllie@users.noreply.github.com>
2025-06-12 18:43:06 +0100

    Hydra folder restructure

--- @AH-64D Apache Longbow/Addons/fza_ah64_hydra/CfgNonAIVehicles.hpp
+++ @AH-64D Apache Longbow/Addons/fza_ah64_hydra/CfgNonAIVehicles.hpp
@@ -3,40 +3,40 @@ class CfgNonAIVehicles {
  
     class Proxyfza_hydra_m151: ProxyWeapon
     {
-        model = "\fza_ah64_model\fza_hydra_m151.p3d";
+        model = "\fza_ah64_hydra\fza_hydra_m151.p3d";
         simulation = "maverickweapon";
     };
     class Proxyfza_hydra_m255: ProxyWeapon
     {
-        model = "\fza_ah64_model\fza_hydra_m261.p3d";
+        model = "\fza_ah64_hydra\fza_hydra_m261.p3d";
         simulation = "maverickweapon";
     };
     class Proxyfza_hydra_m257: ProxyWeapon
     {
-        model = "\fza_ah64_model\fza_hydra_m261.p3d";
+        model = "\fza_ah64_hydra\fza_hydra_m261.p3d";
         simulation = "maverickweapon";
     };
     class Proxyfza_hydra_m261: ProxyWeapon
     {
-        model = "\fza_ah64_model\fza_hydra_m261.p3d";
+        model = "\fza_ah64_hydra\fza_hydra_m261.p3d";
         simulation = "maverickweapon";
     };
     class Proxyfza_hydra_m278: ProxyWeapon
     {
-        model = "\fza_ah64_model\fza_hydra_m261.p3d";
+        model = "\fza_ah64_hydra\fza_hydra_m261.p3d";
         simulation = "maverickweapon";
     };
     
     class Proxyfza_pod_zoneA: ProxyWeapon {
-        model = \fza_ah64_model\weps\pylons\fza_pod_zoneA.p3d;
+        model = \fza_ah64_hydra\data\pylon\fza_pod_zoneA.p3d;
         simulation = "pylonpod";
     };
     class Proxyfza_pod_zoneB: ProxyWeapon {
-        model = \fza_ah64_model\weps\pylons\fza_pod_zoneB.p3d;
+        model = \fza_ah64_hydra\data\pylon\fza_pod_zoneB.p3d;
         simulation = "pylonpod";
     };
     class Proxyfza_pod_zoneE: ProxyWeapon {
-        model = \fza_ah64_model\weps\pylons\fza_pod_zoneE.p3d;
+        model = \fza_ah64_hydra\data\pylon\fza_pod_zoneE.p3d;
         simulation = "pylonpod";
     };
 };
