2d18a30f3b28025a7918ce37eae263a5ae0aa05f
fire-391 <36144022+fire-391@users.noreply.github.com>
2026-03-21 17:16:07 -0400

    added more vehicles

--- @AH-64D Apache Longbow/Addons/fza_compat_racs/CfgVehicles.hpp
+++ @AH-64D Apache Longbow/Addons/fza_compat_racs/CfgVehicles.hpp
@@ -1,7 +1,97 @@
 class CfgVehicles {
     class APC_Tracked_02_base_F;
+    class Tank_F;
+    class StaticCannon;
+    class StaticMGWeapon;
+    class Wheeled_APC_F;
+    //SLA
     class PRACS_SLA_1S91: APC_Tracked_02_base_F {
-        fza_aseClass = "SA_6";
+        fza_aseClass = "RADAR";
+        fza_aseIcon = "TR";
+    };
+    class PRACS_P37_Radar: StaticCannon {
+        fza_aseClass = "RADAR";
+        fza_aseIcon = "SR";
+    };
+    class PRACS_SLA_ZSU23: APC_Tracked_02_base_F {
+        fza_aseClass = "ZSU";
+        fza_aseIcon = "ZU";
+    };
+    class PRACS_SLA_Sa2: StaticMGWeapon {
+        fza_aseClass = "SA2";
+        fza_aseIcon = "SA2";
+    };
+    class PRACS_SLA_SA6: APC_Tracked_02_base_F {
+        fza_aseClass = "SA6";
         fza_aseIcon = "SA6";
+    };
+    class PRACS_SLA_SA8: Wheeled_APC_F {
+        fza_aseClass = "SA8";
+        fza_aseIcon = "SA8";
+    };
+    class PRACS_SLA_SA9: APC_Tracked_02_base_F {
+        fza_aseClass = "SA9";
+        fza_aseIcon = "SA9";
+    };
+    class PRACS_SLA_SA13: Tank_F {
+        fza_aseClass = "SA13";
+        fza_aseIcon = "SA13";
+    };
+    class PRACS_SLA_SA17: Tank_F {
+        fza_aseClass = "SA17";
+        fza_aseIcon = "SA17";
+    };
+    class PRACS_SLA_2S6M_Tunguska: Tank_F {
+        fza_aseClass = "SA19";
+        fza_aseIcon = "SA19";
+    };
+    //RACS
+    class PRACS_MPQ64_Sentinel: Tank_F {
+        fza_aseClass = "RADAR";
+        fza_aseIcon = "SR";
+    };
+    class PRACS_M548_MPQ64: Tank_F {
+        fza_aseClass = "RADAR";
+        fza_aseIcon = "SR";
+    };
+    class PRACS_BMR_AD: Wheeled_APC_F {
+        fza_aseClass = "SAM CROTALE";
+        fza_aseIcon = "CT";
+    };
+    class PRACS_M163_MACHBET: APC_Tracked_02_base_F {
+        fza_aseClass = "SAM VULCAN";
+        fza_aseIcon = "VU";
+    };
+    class PRACS_M163_PVADS: APC_Tracked_02_base_F {
+        fza_aseClass = "SAM VULCAN";
+        fza_aseIcon = "VU";
+    };
+    class PRACS_M163_VADS: APC_Tracked_02_base_F {
+        fza_aseClass = "SAM VULCAN";
+        fza_aseIcon = "VU";
+    };
+    class PRACS_MiM23_HAWK: Tank_F {
+        fza_aseClass = "SAM HAWK";
+        fza_aseIcon = "HK";
+    };
+    class PRACS_M727_IHAWK: Tank_F {
+        fza_aseClass = "SAM HAWK";
+        fza_aseIcon = "HK";
+    };
+    class PRACS_M727_SEM_IHAWK: Tank_F {
+        fza_aseClass = "SAM HAWK";
+        fza_aseIcon = "HK";
+    };
+    class PRACS_M985_AD: Tank_F {
+        fza_aseClass = "SAM STINGER";
+        fza_aseIcon = "ST";
+    };
+    class PRACS_m48_chapparal: Tank_F {
+        fza_aseClass = "SAM CHAPPARAL";
+        fza_aseIcon = "CH";
+    };
+    class PRACS_m48_chapparal_EH: Tank_F {
+        fza_aseClass = "SAM CHAPPARAL";
+        fza_aseIcon = "CH";
     };
 };
