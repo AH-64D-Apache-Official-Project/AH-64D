261363da8a4ffe01999220d769ca54c7e30ff259
fire-391 <36144022+fire-391@users.noreply.github.com>
2026-03-21 17:50:53 -0400

    Seperated racs forses from sla

--- @AH-64D Apache Longbow/Addons/fza_compat_pracs/CfgVehicles.hpp
+++ @AH-64D Apache Longbow/Addons/fza_compat_pracs/CfgVehicles.hpp
@@ -1,51 +1,8 @@
 class CfgVehicles {
     class APC_Tracked_02_base_F;
     class Tank_F;
-    class StaticCannon;
-    class StaticMGWeapon;
     class Wheeled_APC_F;
-    //SLA
-    class PRACS_SLA_1S91: APC_Tracked_02_base_F {
-        fza_aseClass = "RADAR";
-        fza_aseIcon = "TR";
-    };
-    class PRACS_P37_Radar: StaticCannon {
-        fza_aseClass = "RADAR";
-        fza_aseIcon = "SR";
-    };
-    class PRACS_SLA_ZSU23: APC_Tracked_02_base_F {
-        fza_aseClass = "ZSU";
-        fza_aseIcon = "ZU";
-    };
-    class PRACS_SLA_Sa2: StaticMGWeapon {
-        fza_aseClass = "SA2";
-        fza_aseIcon = "SA2";
-    };
-    class PRACS_SLA_SA6: APC_Tracked_02_base_F {
-        fza_aseClass = "SA6";
-        fza_aseIcon = "SA6";
-    };
-    class PRACS_SLA_SA8: Wheeled_APC_F {
-        fza_aseClass = "SA8";
-        fza_aseIcon = "SA8";
-    };
-    class PRACS_SLA_SA9: APC_Tracked_02_base_F {
-        fza_aseClass = "SA9";
-        fza_aseIcon = "SA9";
-    };
-    class PRACS_SLA_SA13: Tank_F {
-        fza_aseClass = "SA13";
-        fza_aseIcon = "SA13";
-    };
-    class PRACS_SLA_SA17: Tank_F {
-        fza_aseClass = "SA17";
-        fza_aseIcon = "SA17";
-    };
-    class PRACS_SLA_2S6M_Tunguska: Tank_F {
-        fza_aseClass = "SA19";
-        fza_aseIcon = "SA19";
-    };
-    //RACS
+    
     class PRACS_MPQ64_Sentinel: Tank_F {
         fza_aseClass = "RADAR";
         fza_aseIcon = "SR";
---
+++ @AH-64D Apache Longbow/Addons/fza_compat_pracs_sla/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches
+{
+    class fza_compat_racs_sla
+    {
+        units[] = {};
+        author = "AH-64D Development Team";
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"fza_ah64_controls", "PRACS_SLA_Mod"};
+        skipWhenMissingDependencies = 1;
+    };
+};
+
+#include "CfgVehicles.hpp"
