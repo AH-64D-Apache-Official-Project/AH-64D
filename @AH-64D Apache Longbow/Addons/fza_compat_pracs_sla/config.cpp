9d434cd0274f12153b0fa8706b0efa449afe7548
fire-391 <36144022+fire-391@users.noreply.github.com>
2026-03-22 10:47:40 -0400

    Correct requiredAddons

--- @AH-64D Apache Longbow/Addons/fza_compat_pracs_sla/config.cpp
+++ @AH-64D Apache Longbow/Addons/fza_compat_pracs_sla/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches
         author = "AH-64D Development Team";
         weapons[] = {};
         requiredVersion = 1.0;
-        requiredAddons[] = {"fza_ah64_controls", "PRACS_SLA_Mod"};
+        requiredAddons[] = {"fza_ah64_controls", "PRACS_Core"};
         skipWhenMissingDependencies = 1;
     };
 };
